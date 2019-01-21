5db258f51e8897800f8ac330827efd0e8c2c0efa
Elias Norrby <elias.norrby@gmail.com>
2019-01-21 09:53:18 +0100

    Update setup instructions

--- scripts/cmds-run-on-server.sh
+++ scripts/cmds-run-on-server.sh
@@ -11,7 +11,25 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 # Results in different zplug home than on mac...
 
 # VIM - badwolf colors, surround plugins
+## vim surround
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/surround.git
+vim -u NONE -c "helptags surround/doc" -c q
 
+## vim commentary
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/commentary.git
+vim -u NONE -c "helptags commentary/doc" -c q
+
+## vim repeat
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/repeat.git
+
+## badwolf scheme
+# copy colors folder to ~/.vim
 
 # Dotfiles
 # Just clone private repo, run setup_symlinks.sh
