b3cf4dd22f0a6f80149451f23a621462ff7550a4
Elias Norrby <elias.norrby@gmail.com>
2019-04-07 16:27:29 +0200

    Add Postman to hyper-apps

--- keyboard/hammerspoon/hyper-apps.lua
+++ keyboard/hammerspoon/hyper-apps.lua
@@ -1,15 +1,14 @@
 -- Keybindings for launching apps in Hyper Mode
 return {
-  -- { 'b', 'Google Chrome' },     	-- "B" for "Browser"
-  -- { 'c', 'Slack' },             	-- "C for "Chat"
-  { 'b', 'Bear' },     				-- "B" for "Bear"
-  { 'c', 'Google Chrome' },         -- "C for "Chrome"
-  { 'o', 'OmniFocus' }, 			-- "D" for "Do!" ... or "Done!"
-  { 'e', 'Visual Studio Code' },	-- "E" for "Editor"
-  { 'f', 'Fantastical 2' },         -- "F" for "Fantastical"
-  { 's', 'Sublime Text' },          -- "S" for "Sublime"
-  { 't', 'iTerm' },             	-- "T" for "Terminal"
-  { 'd', 'Drafts' },            	-- "D" for "Drafts"
-  { 'm', 'Spark'},              -- "E" for "Email"
+  { 'b', 'Bear' },
+  { 'c', 'Google Chrome' },
+  { 'o', 'OmniFocus' },
+  { 'e', 'Visual Studio Code' },
+  { 'f', 'Fantastical 2' },
+  { 's', 'Sublime Text' },
+  { 't', 'iTerm' },
+  { 'd', 'Drafts' },
+  { 'm', 'Spark'},
   { 'q', 'MySQLWorkbench'},
+  { 'n', 'Postman'},
 }
