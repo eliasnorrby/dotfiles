e24b84b754d25ac3cac5b3ac238fddc8d6dced2a
Elias Norrby <elias.norrby@gmail.com>
2019-07-09 09:24:27 +0200

    emacs: Add and disable web packages

--- doom.d/config.el
+++ doom.d/config.el
@@ -2,7 +2,18 @@
 ;; Place your private configuration here
 
 ;; Change the font!
-(setq doom-font (font-spec :family "Source Code Pro" :size 18))
+(setq doom-font (font-spec :family "Source Code Pro" :size 12))
+
+;; keybindings
+;; use the map! macro to bind things,
+;; and unmap! to unmap.
+;; example: (unmap! 'motion "g s l")
+(map! :map 'evilem-map
+      "l" #'avy-goto-line
+      "c" #'avy-goto-char-timer
+      (:prefix "d"
+        "l" #'avy-kill-whole-line
+        "r" #'avy-kill-region))
 
 ;; avy
 ;; Make avy target all windows
@@ -13,17 +24,26 @@
 ;; TODO add .projectile files to all projects in ~/learn
 (setq  projectile-project-search-path '("~/dev" "~/learn" "~/sandbox"))
 
-;; company
-;; Disable suggestions in org-mode
-(setq company-global-modes '(not org-mode))
+;; org
+;; Disable company suggestions in org-mode
+;; (setq company-global-modes '(not org-mode))
+
+;; Hide inline code markers
+(setq org-hide-emphasis-markers t)
+
+;; Set completion time on DONE toggle
+(setq org-log-done 'time)
 
 ;; which-key
 ;; Make which-key display sooner. I should make it slower as I learn more, but
 ;; having it be quick helps discoverability in the beginning.
-(setq which-key-idle-delay 0.1)
+(setq which-key-idle-delay 0.4)
 
 ;; filetype specifics
 (setq js-indent-level 2)
+
+;; web mode
+;; (add-to-list 'auto-mode-alist )
 
 ;; minor mode hooks
 ;; I think this is the way to enable the prettier minor mode for the files I
@@ -33,3 +53,4 @@
 (add-hook 'js2-mode-hook 'prettier-js-mode)
 ;; (add-hook 'js-jsx-mode-hook 'prettier-js-mode)
 (add-hook 'yaml-mode-hook 'prettier-js-mode)
+(add-hook 'mhtml-mode-hook 'prettier-js-mode)
