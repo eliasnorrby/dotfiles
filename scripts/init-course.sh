68c4667ff8d45df00f17c56642477f9f8bc02416
Elias Norrby <elias.norrby@gmail.com>
2019-07-11 14:54:10 +0200

    scripts: Add init-course.sh
    init-course.sh is a script for bootstrapping the file structure I aim to
    use for courses I take.

---
+++ scripts/init-course.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+ORGANIZATION="en-learn"
+REPO="${PWD##*/}"
+LECTURE_COUNTER=lecture_counter
+COURSE_MATERIALS=_course-materials
+
+if [ ! -d ".git" ] ; then
+    echo "No .git folder found, initializing repo"
+    git init
+fi
+
+if [ ! -f "$LECTURE_COUNTER" ] ; then
+  echo "Setting up lecture counter"
+  echo "1" > $LECTURE_COUNTER
+  git add !$
+fi
+
+if [ ! -f .gitignore ] || ! grep "$COURSE_MATERIALS" .gitignore ; then
+  echo "Appending $COURSE_MATERIALS to .gitignore"
+  echo "$COURSE_MATERIALS" >> .gitignore
+  git add !$
+fi
+
+echo "Committing changes"
+git commit -m "Initialize course"
+
+read -p "Create new repository $ORGANIZATION/$REPO on GitHub? [y/n] " CHOICE
+
+case $CHOICE in
+  [yY]|[yY][eE][sS])
+    echo "Creating remote repo"
+    hub create "$ORGANIZATION/$REPO"
+    echo "Pushing to remote"
+    git push --set-upstream origin master
+    ;;
+  [nN]|[nN][oO])
+    echo "Skipping creating repo"
+    ;;
+  *)
+    echo "Invalid choice, skipping"
+    ;;
+esac
+
+cat <<EOF
+
+  Course initialization complete.
+
+  Consider creating a README.md to describe the course.
+
+EOF
