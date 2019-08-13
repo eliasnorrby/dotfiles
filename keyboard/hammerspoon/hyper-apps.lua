087f26f66a536daeb5cd92ce49d7b2b42168dee2
Elias Norrby <elias.norrby@gmail.com>
2019-08-13 16:01:49 +0200

    hammerspoon: Clean up mnemonics

--- keyboard/hammerspoon/hyper-apps.lua
+++ keyboard/hammerspoon/hyper-apps.lua
@@ -1,18 +1,17 @@
 -- Keybindings for launching apps in Hyper Mode
 return {
-  { 'b', 'Bear' },     				-- "B" for "Bear"
-  { 'c', 'Google Chrome' },         -- "C for "Chrome"
-  { 'o', 'OmniFocus' }, 			-- "D" for "Do!" ... or "Done!"
-  { 'e', 'Visual Studio Code' },	-- "E" for "Editor"
-  { 'f', 'Fantastical 2' },         -- "F" for "Fantastical"
-  { 's', 'Emacs' },          -- "S" for "Sublime"
-  { 'g', 'Things3' },
-  -- { 't', 'iTerm' },             	-- "T" for "Terminal"
-  { 't', 'Alacritty' },             	-- "T" for "Terminal"
-  { 'd', 'Drafts' },            	-- "D" for "Drafts"
-  { 'm', 'Gmail'},              -- "E" for "Email"
-  { 'q', 'MySQLWorkbench'},
+  { 'b', 'Bear' },                 -- "B" for "Bear"
+  { 'c', 'Google Chrome' },        -- "C" for "Chrome"
+  { 'o', 'OmniFocus' },            -- "O" for "OmniFocus"
+  { 'e', 'Visual Studio Code' },   -- "E" for "Editor"
+  { 'f', 'Fantastical 2' },        -- "F" for "Fantastical"
+  { 's', 'Emacs' },                -- "S" for "Sublime"
+  { 'g', 'Things3' },              -- "G" fro "GTD"
+  { 't', 'Alacritty' },            -- "T" for "Terminal"
+  { 'd', 'Drafts' },               -- "D" for "Drafts"
+  { 'm', 'Gmail'},                 -- "M" for "Mail"
+  { 'q', 'MySQLWorkbench'},        -- "Q" for "sQl"
   { 'n', 'Postman'},
-  { 'w', 'MacVim'},
-  { 'i', 'IntelliJ IDEA CE'}
+  { 'w', 'MacVim'},                -- "W" for "Wiki"
+  { 'i', 'IntelliJ IDEA CE'}       -- "I" for "IntelliJ"
 }
