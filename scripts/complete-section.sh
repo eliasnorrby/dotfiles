bc56a92bb4696a685722948bfcd56dc49bdd1af6
Elias Norrby <elias.norrby@gmail.com>
2019-08-27 18:27:38 +0200

    scripts: Add lecture number as argument to cs
    complete-section.sh now takes the current lecture number as the first argument.

--- scripts/complete-section.sh
+++ scripts/complete-section.sh
@@ -9,6 +9,7 @@ if [ ! -d .git ] ; then
   cd "$PROJECT_ROOT"
 fi
 
+ARG_NUMBER=$1
 
 ## Variables
 LECTURE_COUNTER=$(pwd)/lecture_counter
@@ -37,11 +38,21 @@ read_lecture_counter() {
 increment_counter() {
   if [ ! -f $LECTURE_COUNTER ] ; then
     echo "No counter file found. Creating..."
+  fi
+
+  if [ ! -z "$ARG_NUMBER" ] && validate_number $ARG_NUMBER ; then
+    echo $ARG_NUMBER > $LECTURE_COUNTER
+    return
+  else
+    echo "That's not a number: $ARG_NUMBER"
+  fi
+
+  if [ ! -f $LECTURE_COUNTER ] || [ ! -z "$ARG_NUMBER" ] ; then
     local INPUT_NUMBER=""
     while [ -z "$INPUT_NUMBER" ] || ! validate_number $INPUT_NUMBER ; do
       read -p "Which lecture is this? " INPUT_NUMBER
     done
-    echo "Creating new lecture counter: $INPUT_NUMBER"
+    echo "Setting lecture counter to: $INPUT_NUMBER"
     echo $INPUT_NUMBER > $LECTURE_COUNTER
   else
     local INPUT_NUMBER=$(read_lecture_counter)
@@ -67,33 +78,28 @@ trap cleanup ERR SIGINT
 ## 1: Read or otherwise determine lecture number
 increment_counter
 
-LECTURE_NUMBER=$(read_lecture_counter)
 
-if [ ! -z "$1" ] ; then
-  MSG="$1"
+LECTURE_NUMBER=$(read_lecture_counter)
+BASE_MSG="Enter a description"
+VIM_MSG="type 'v' to edit in vim"
+if [ -z "$LECTURE_NUMBER" ] ; then
+  # This branch shouldn't happen any more
+  EXTENDED_MSG="e.g. 'Complete lecture 12'"
+  while [ -z "$MSG" ]; do
+    read -p "$BASE_MSG ($EXTENDED_MSG): " MSG
+  done
 else
-  LECTURE_NUMBER=$(read_lecture_counter)
-  BASE_MSG="Enter a description"
-  VIM_MSG="type 'v' to edit in vim"
-  if [ -z "$LECTURE_NUMBER" ] ; then
-    # This branch shouldn't happen any more
-    EXTENDED_MSG="e.g. 'Complete lecture 12'"
-    while [ -z "$MSG" ]; do
-      read -p "$BASE_MSG ($EXTENDED_MSG): " MSG
-    done
+  # DEFAULT_MSG="Complete lecture $((LECTURE_NUMBER+1))"
+  DEFAULT_MSG="${DEFAULT_MSG_BASE}${LECTURE_NUMBER}"
+  EXTENDED_MSG="default: '$DEFAULT_MSG'"
+  read -p "$BASE_MSG ($EXTENDED_MSG) ($VIM_MSG): " MSG
+  if [ -z "$MSG" ] ; then
+    MSG=$DEFAULT_MSG
+  elif [ "$MSG" == "v" ] ; then
+    OPT_EDIT_MSG=true
+    MSG="<to be edited in vim>"
   else
-    # DEFAULT_MSG="Complete lecture $((LECTURE_NUMBER+1))"
-    DEFAULT_MSG="${DEFAULT_MSG_BASE}${LECTURE_NUMBER}"
-    EXTENDED_MSG="default: '$DEFAULT_MSG'"
-    read -p "$BASE_MSG ($EXTENDED_MSG) ($VIM_MSG): " MSG
-    if [ -z "$MSG" ] ; then
-      MSG=$DEFAULT_MSG
-    elif [ "$MSG" == "v" ] ; then
-      OPT_EDIT_MSG=true
-      MSG="<to be edited in vim>"
-    else
-      MSG="${DEFAULT_MSG}: $MSG"
-    fi
+    MSG="${DEFAULT_MSG}: $MSG"
   fi
 fi
 
@@ -134,7 +140,7 @@ case $CHOICE in
   [yY]|[yY][eE][sS])
     echo "Committing changes"
     if [ "$OPT_EDIT_MSG" == true ] ; then
-      git commit -em "${DEFAULT_MSG}: "
+      git commit -vem "${DEFAULT_MSG}: "
     else
       git commit -m "$MSG"
     fi
