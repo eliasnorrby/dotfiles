18900d301d1092226f7b944594b82c2dc8835d0a
Elias Norrby <elias.norrby@gmail.com>
2019-11-10 10:14:03 +0100

    emacs: minor edits and updates

--- doom.d/config.el
+++ doom.d/config.el
@@ -11,12 +11,16 @@
 ;; (setq doom-font (font-spec :family "Source Code Pro" :size 14))
 ;; (setq doom-font (font-spec :family "mononoki Nerd Font" :size 14))
 ;; (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 14))
-(setq doom-font (font-spec :family "Iosevka Nerd Font" :weight 'normal :size 16))
+(setq doom-font (font-spec :family "Iosevka Nerd Font" :weight 'light :size 16))
 ;; (setq doom-font (font-spec :family "MesloLGMDZ Nerd Font" :size 14))
 ;; (setq doom-font (font-spec :family "Fira Code Retina" :size 14))
 
 ;; Set the theme
 (setq doom-theme 'doom-palenight)
+
+;; line numbers
+;; TODO I need to find a way to only enable this in code buffers
+;; (setq-default display-line-numbers 'relative)
 
 ;; keybindings
 ;; use the map! macro to bind things,
@@ -36,8 +40,7 @@
 
 ;; projectile
 ;; Look for projects in these folders
-;; TODO add .projectile files to all projects in ~/learn
-(setq projectile-project-search-path '("~/dev" "~/learn" "~/sandbox"))
+(setq projectile-project-search-path '("~/dev/" "~/learn/" "~/sandbox/"))
 
 ;; org
 ;; Disable company suggestions in org-mode
@@ -49,13 +52,23 @@
 ;; Set completion time on DONE toggle
 (setq org-log-done 'time)
 
+;; Try to avoid accidental edits
+(setq-default org-catch-invisible-edits 'error)
+(setq-default org-ctrl-k-protect-subtree 'error)
+
+;; org keybindings
+(map! :map org-mode-map
+      :localleader
+      ;; "m" #'org-toggle-narrow-to-subtree
+      "SPC" #'org-narrow-to-subtree
+      "m" #'widen)
+
+(add-hook 'org-mode-hook (lambda () (setq display-line-numbers nil)))
+
 ;; which-key
 ;; Make which-key display sooner. I should make it slower as I learn more, but
 ;; having it be quick helps discoverability in the beginning.
 (setq which-key-idle-delay 0.4)
-
-;; filetype specifics
-(setq js-indent-level 2)
 
 ;; web mode
 ;; (add-to-list 'auto-mode-alist )
@@ -63,20 +76,39 @@
 ;; minor mode hooks
 ;; I think this is the way to enable the prettier minor mode for the files I
 ;; want to auto-format
-(add-hook 'json-mode-hook 'prettier-js-mode)
+;; (add-hook 'json-mode-hook 'prettier-js-mode)
+;; (add-hook 'js2-mode-hook 'prettier-js-mode)
+;; (add-hook 'yaml-mode-hook 'prettier-js-mode)
+;; (add-hook 'mhtml-mode-hook 'prettier-js-mode)
+
+;; (add-hook 'web-mode-hook 'prettier-js-mode)
 ;; (add-hook 'javascript-mode-hook 'prettier-js-mode)
-(add-hook 'js2-mode-hook 'prettier-js-mode)
 ;; (add-hook 'js-jsx-mode-hook 'prettier-js-mode)
-(add-hook 'yaml-mode-hook 'prettier-js-mode)
-(add-hook 'mhtml-mode-hook 'prettier-js-mode)
 
+;; stolen from https://github.com/ar1a/dotfiles/blob/master/emacs/.doom.d/config.el
+(after! emmet-mode
+  ;; this is already done in emmet :config but it doesn't seem to get set on my
+  ;; machine, so let's do it again
+  (map! :map emmet-mode-keymap
+        :v [tab] #'emmet-wrap-with-markup
+        ;; :i [tab] #'+web/indent-or-yas-or-emmet-expand
+        :i "C-e" #'emmet-expand-line))
 
-(define-derived-mode web-jsx-mode web-mode "WebJsx"
-  "Major mode for editing web jsx templates."
-  (web-mode)
-  (setq web-mode-content-type "jsx"))
+;;; Bug fixing
+;; Henrik told me to try this snippet to fix broken session reestores
+;; after straight was merged into develop. I don't know how long I need to keep
+;; it here.
+; (after! persp-mode
+;   (remove-hook 'persp-filter-save-buffers-functions #'buffer-live-p)
 
-(define-derived-mode web-css-mode web-mode "WebCss"
-  "Major mode for editing web css templates."
-  (web-mode)
-  (setq web-mode-content-type "css"))
+;   (defun +workspaces-dead-buffer-p (buf)
+;     (not (buffer-live-p buf)))
+;   (add-hook 'persp-filter-save-buffers-functions #'+workspaces-dead-buffer-p))
+
+;;; Optimizations
+;; Somebody said scrolling was slow and henrik suggested this:
+;; (setq fast-but-imprecise-scrolling nil)
+;; (setq inhibit-compacting-font-caches nil)
+
+;; Also, this could help with flickering
+;; (add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
--- doom.d/init.el
+++ doom.d/init.el
@@ -13,6 +13,9 @@
 (setenv "PATH" (concat "/Users/elias.norrby/.nvm/versions/node/v10.15.1/bin:" (getenv "PATH")))
 (setq exec-path (append exec-path '("/Users/elias.norrby/.nvm/versions/node/v10.15.1/bin")))
 
+(setq doom-leader-alt-key "M-m"
+      doom-localleader-alt-key "M-m m")
+
 (doom! :input
        ;;chinese
        ;;japanese
@@ -29,7 +32,8 @@
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
-       hl-todo           ; highlight TODO/FIXME/NOTE tags
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       ;;hydra
        ;;indent-guides     ; highlighted indent columns
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink the current line after jumping
@@ -39,8 +43,8 @@
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
        ;;pretty-code       ; replace bits of code with pretty symbols
-       ;;tabbar            ; FIXME an (incomplete) tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       ;;tabs              ; an tab bar for Emacs
+       ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -51,11 +55,10 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
+       ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
-       ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
 
@@ -70,14 +73,14 @@
        :term
        ;;eshell            ; a consistent, cross-platform shell (WIP)
        ;;term              ; terminals in Emacs
-       ;;vterm             ; another terminals in Emacs
+       vterm             ; another terminals in Emacs
 
        :tools
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        eval              ; run code, run (also, repls)
        flycheck          ; tasing you for every semicolon you forget
