4af9f77508df7dbaca45752aec402e4d76295368
Elias Norrby <elias.norrby@gmail.com>
2019-11-10 10:08:55 +0100

    hammerspoon: add outlook and firefox app mappings

--- keyboard/hammerspoon/hyper-apps.lua
+++ keyboard/hammerspoon/hyper-apps.lua
@@ -2,16 +2,18 @@
 return {
   { 'b', 'Bear' },                 -- "B" for "Bear"
   { 'c', 'Google Chrome' },        -- "C" for "Chrome"
-  { 'o', 'OmniFocus' },            -- "O" for "OmniFocus"
+  { 'o', 'Outlook' },              -- "O" for "Outlook"
   { 'e', 'Visual Studio Code' },   -- "E" for "Editor"
   { 'f', 'Fantastical 2' },        -- "F" for "Fantastical"
   { 's', 'Emacs' },                -- "S" for "Sublime"
-  { 'g', 'Things3' },              -- "G" fro "GTD"
+  { 'g', 'Things3' },              -- "G" for "GTD"
   { 't', 'Alacritty' },            -- "T" for "Terminal"
   { 'd', 'Drafts' },               -- "D" for "Drafts"
   { 'm', 'Gmail'},                 -- "M" for "Mail"
   { 'q', 'MySQLWorkbench'},        -- "Q" for "sQl"
   { 'n', 'Postman'},
   { 'w', 'MacVim'},                -- "W" for "Wiki"
-  { 'i', 'IntelliJ IDEA CE'}       -- "I" for "IntelliJ"
+  { 'i', 'IntelliJ IDEA CE'},      -- "I" for "IntelliJ"
+  { 'u', 'Spotify' },              -- "U" for some reason
+  { 'x', 'Firefox Developer Edition' },
 }
