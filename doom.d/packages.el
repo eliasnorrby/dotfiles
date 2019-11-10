390acec84c2d976c1770ab6907b022a980972a81
Elias Norrby <elias.norrby@gmail.com>
2019-11-10 10:13:02 +0100

    emacs: disable custom packages

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -5,7 +5,9 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
-(package! prettier-js)
-(package! ansible)
-(package! web-mode)
+
+;; (package! prettier-js)
+;; (package! ansible)
+;; (package! web-mode)
+
 ;; (package! emmet-mode)
