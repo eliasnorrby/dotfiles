54642b3d71ccf86c4635056d3b4d35f6a46288ea
Elias Norrby <elias.norrby@gmail.com>
2019-11-29 08:55:34 +0100

    fix(editor): don't redirect stderr in vim-plug-install
    For some reason it breaks the PlugInstall command. Maybe it's due to one
    of the plugins misbehaving?

--- ansible/roles/editor/files/vim-plug-install.sh
+++ ansible/roles/editor/files/vim-plug-install.sh
@@ -4,9 +4,8 @@ STATUS_LOG=$(vim +PlugStatus +qall 2>/dev/null)
 grep -o "Not found. Try" <<< "$STATUS_LOG" > /dev/null
 
 if [ "$?" -eq 0 ] ; then
-  vim +'PlugInstall --sync' +qall > /dev/null 2>&1
-  echo "changed"
+  vim +'PlugInstall --sync' +qall > /dev/null
+  echo -n "changed"
 else
-  echo "unchanged"
+  echo -n "unchanged"
 fi
-
