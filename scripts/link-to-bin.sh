f2f7f533d3b08954ae33f389049e30d133257786
Elias Norrby <elias.norrby@gmail.com>
2019-12-11 22:33:16 +0100

    feat(scripts): default to src without file ending

--- scripts/link-to-bin.sh
+++ scripts/link-to-bin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SRC=$1
-DEST=${2:-$SRC}
+DEST=${2:-${SRC%%.*}}
 
 if [ -z "$SRC" ] || ! ls $SRC >/dev/null 2>&1 || echo $SRC $DEST | grep "/" >/dev/null 2>&1 ; then
   echo "Invalid arguments: $1 , $2"
