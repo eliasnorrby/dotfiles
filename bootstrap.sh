a4d7d41cbc469077f41c31c7170e9372506db747
Elias Norrby <elias.norrby@gmail.com>
2019-12-13 12:37:24 +0100

    refactor: merge playbook with dotfiles
    BREAKING_CHANGE: dotfile provisioning is now handled by ansible
    configuration stored in dotfiles/_provision. The dotfiles ansible role
    responsible for setting up links is at dotfiles_provision/roles/.
    
    The main entrypointis _provision/playbook.yml. It's much cleaner now,
    sourcing dotfiles/homebrew/mas roles in order, with variable setup and
    topic specific tasks set up in the dotfiles role.
    
    Travis configuration rewritten to test the different modules separately.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,21 +6,26 @@
 
 SECONDS=0
 
+if [ -z "$TARGET_BRANCH" ] ; then
+  TARGET_BRANCH=${1:-master}
+fi
+
 set -exo pipefail
 
+export DOTFILES_REPO="https://github.com/eliasnorrby/dotfiles"
+export TARBALL_URL="$DOTFILES_REPO/tarball/$TARGET_BRANCH"
 export DOTFILES=~/.dotfiles
 
 _msg() { printf "\r\033[2K\033[0;32m[ .. ] %s\033[0m\n" "$*"; }
 
 _get_repo_snapshot() {
-  local url="https://github.com/eliasnorrby/mac-dev-playbook/tarball/master"
-  curl -sL $url | tar xz
+  curl -sL $TARBALL_URL | tar xz
 }
 
-_msg "Downloading playbook from eliasnorrby/mac-dev-playbook@master..."
-cd $(mkdtemp -d)
+_msg "Downloading repository snapshot from eliasnorrby/dotfiles@$TARGET_BRANCH..."
+cd $(mktemp -d)
 _get_repo_snapshot
-cd eliasnorrby-mac-dev-playbook*
+cd eliasnorrby-dotfiles*
 
 _msg "Installing pip..."
 sudo easy_install pip
@@ -29,10 +34,11 @@ _msg "Installing ansible..."
 sudo pip install ansible
 
 _msg "Installing playbook requirements..."
+cd _provision
 ansible-galaxy install -r requirements.yml
 
 _msg "Running the playbook..."
-ansible-playbook main.yml --tags 'all,do_homebrew' -v
+ansible-playbook playbook.yml --tags 'all,do_homebrew,do_packages,do_defaults' -v
 
 _msg "Run post-install script..."
 cd $DOTFILES
---
+++ dev/ansible/aliases.zsh
@@ -0,0 +1,1 @@
+alias ap="ansible-playbook"
