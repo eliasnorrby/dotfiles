7e36f87f62d7b06b52e14dcd038e865576b75aa9
Elias Norrby <elias.norrby@gmail.com>
2019-12-16 10:05:57 +0100

    feat(emacs): enable ansible module

--- editor/emacs/doom/init.el
+++ editor/emacs/doom/init.el
@@ -79,7 +79,7 @@
        vterm             ; another terminals in Emacs
 
        :tools
-       ;;ansible
+       ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
