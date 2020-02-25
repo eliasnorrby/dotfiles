67c2b5ac031aae8b7a235b5db82f6258cf5493f8
Elias Norrby <elias.norrby@gmail.com>
2020-02-25 08:52:21 +0100

    feat(provision): update npm packages in update-all

--- _provision/update-all.zsh
+++ _provision/update-all.zsh
@@ -48,5 +48,24 @@ echo-ok "doom upgraded!"
 echo-info "Wrote logs to $doom_log"
 echo "$SPACE"
 
-# TODO: global npm/gem/pip packages
+# npm
+npm_log=$(mktemp)
+echo "$SPACE"
+echo-info "Upgrading global npm packages..."
+npm upgrade -g | tee $npm_log
+echo-ok "npm packages upgraded!"
+echo-info "Wrote logs to $npm_log"
+echo "$SPACE"
+
+# TODO: global gem/pip packages
 # TODO: grep logs to find number of packages updated
+
+# finish up
+echo-info "Logs available at:"
+echo "  zplug: $zplug_log"
+echo "  vim_plug: $vim_plug_log"
+echo "  brew: $brew_log"
+echo "  doom: $doom_log"
+echo "  npm: $npm_log"
+echo-ok "Done!"
+
