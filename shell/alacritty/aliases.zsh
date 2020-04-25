5b2ef9a01be20765b4c82fed600d0870868c0ff8
Elias Norrby <elias.norrby@gmail.com>
2020-04-25 12:07:05 +0200

    alacritty: add command for switching theme

--- shell/alacritty/aliases.zsh
+++ shell/alacritty/aliases.zsh
@@ -1,1 +1,10 @@
 alias alac="vim $XDG_CONFIG_HOME/alacritty/alacritty.yml"
+
+function theme() {
+  local theme=${1:-${VIM_COLOR:-${DEFAULT_THEME}}}
+  sed -i'.bak' "s/^colors: \*.*/colors: \*$theme/" "$DOTFILES/shell/alacritty/alacritty.yml"
+}
+
+alias ayu="theme ayu"
+alias gruvbox="theme gruvbox"
+alias palenight="theme palenight"
