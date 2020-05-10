9a376013aa0e51334263cde678b482a3c9cc213a
Elias Norrby <elias.norrby@gmail.com>
2020-05-10 18:05:29 +0200

    vim: use nvim for EDITOR

--- editor/vim/env.zsh
+++ editor/vim/env.zsh
@@ -1,8 +1,5 @@
 export VIM_DIR="$HOME/.vim"
 export VIMINIT="let \$MYVIMRC='$XDG_CONFIG_HOME/vim/.vimrc' | source \$MYVIMRC"
 
-export EDITOR=vim
-export VISUAL=vim
-
 export BAT_THEME=base16
 export VIM_COLOR=palenight
