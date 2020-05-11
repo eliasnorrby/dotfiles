42871e74e35af8505d89ec8f64ce1fb25b5f4d36
Elias Norrby <elias.norrby@gmail.com>
2020-05-11 08:29:42 +0200

    nvim: remove vim stuff from env.zsh

--- editor/neovim/env.zsh
+++ editor/neovim/env.zsh
@@ -1,5 +1,4 @@
-# export VIM_DIR="$XDG_CONFIG_HOME/nvim"
-export VIMINIT="let \$MYVIMRC='$XDG_CONFIG_HOME/nvim/init.vim' | source \$MYVIMRC"
+export VIM_DIR="$XDG_CONFIG_HOME/nvim"
 
 export EDITOR=nvim
 export VISUAL=nvim
