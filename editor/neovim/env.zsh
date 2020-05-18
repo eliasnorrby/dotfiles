3885adc90f03ee40940ce23242d1023a025f1b4e
Elias Norrby <elias.norrby@gmail.com>
2020-05-18 13:37:50 +0200

    vim: move color env vars to nvim env

--- editor/neovim/env.zsh
+++ editor/neovim/env.zsh
@@ -1,4 +1,7 @@
 export VIM_DIR="$XDG_CONFIG_HOME/nvim"
 
+export BAT_THEME=base16
+export VIM_COLOR=palenight
+
 export EDITOR=nvim
 export VISUAL=nvim
--- editor/vim/env.zsh
+++ editor/vim/env.zsh
@@ -2,5 +2,5 @@
 # export VIMINIT="let \$MYVIMRC='$XDG_CONFIG_HOME/vim/.vimrc' | source \$MYVIMRC"
 
 # FIXME: do I use these for vim or nvim?
-export BAT_THEME=base16
-export VIM_COLOR=palenight
+# export BAT_THEME=base16
+# export VIM_COLOR=palenight
