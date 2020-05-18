3265c20a23c4f4144a49e2110ad6c1eea001c87e
Elias Norrby <elias.norrby@gmail.com>
2020-05-18 13:43:51 +0200

    fix(zsh): remove subshells from lang/java/env
    The calls to set up JAVA_HOME were impacting shell startup time. And not
    only that; since it's sourced in .zshenv, it impacts the execution time
    of virtually every zsh script, notably making tmux-navigator slow.

--- lang/java/env.zsh
+++ lang/java/env.zsh
@@ -1,6 +1,9 @@
 if [[ "$(_os)" == "macos" ]]; then
-  export JAVA_8_HOME="$(/usr/libexec/java_home -v 1.8)"
-  export JAVA_13_HOME="$(/usr/libexec/java_home -v 13)"
+  # subshell-elimination: uncomment in case of issues with the static locations
+  # export JAVA_8_HOME="$(/usr/libexec/java_home -v 1.8)"
+  export JAVA_8_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home"
+  # export JAVA_13_HOME="$(/usr/libexec/java_home -v 13)"
+  export JAVA_13_HOME="/Library/Java/JavaVirtualMachines/openjdk-13.0.2.jdk/Contents/Home"
 fi
 
 alias java8='export JAVA_HOME=$JAVA_8_HOME'
