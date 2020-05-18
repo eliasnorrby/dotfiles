fb8287d8eb6d9c03c7e8ece503b0328752b67ddd
Elias Norrby <elias.norrby@gmail.com>
2020-05-18 14:01:31 +0200

    alacritty: remove backup for theme alias

--- shell/alacritty/aliases.zsh
+++ shell/alacritty/aliases.zsh
@@ -2,7 +2,7 @@ alias alac="vim $XDG_CONFIG_HOME/alacritty/alacritty.yml"
 
 function theme() {
   local theme=${1:-${VIM_COLOR:-${DEFAULT_THEME}}}
-  sed -i'.bak' "s/^colors: \*.*/colors: \*$theme/" "$DOTFILES/shell/alacritty/alacritty.yml"
+  sed -i '' "s/^colors: \*.*/colors: \*$theme/" "$DOTFILES/shell/alacritty/alacritty.yml"
 }
 
 alias ayu="theme ayu"
