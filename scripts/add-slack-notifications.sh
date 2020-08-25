9839dce61559e4075e9a318ae7109f0536713710
Elias Norrby <elias.norrby@gmail.com>
2020-08-25 22:51:37 +0200

    feat(scripts): format and solve linting issues
    Hopefully, no errors were introduced. I haven't had the time and energy
    to verify that.

--- scripts/add-slack-notifications.sh
+++ scripts/add-slack-notifications.sh
@@ -10,8 +10,8 @@
 # Put this in a file in the default location or reference it with the
 # -f flag.
 
-DIR=$(dirname $([ -L "$0" ] && readlink -f "$0" || echo $0))
-[ -f $DIR/echos.sh ] && . $DIR/echos.sh || (echo "Could not source 'echos', aborting" && exit 1)
+DIR=$(dirname "$([ -L "$0" ] && readlink -f "$0" || echo "$0")")
+[ -f "$DIR/echos.sh" ] && . "$DIR/echos.sh" || (echo "Could not source 'echos', aborting" && exit 1)
 
 TOKEN_FILE_PATH="$HOME/.travis/slack-token"
 ACCOUNT="eliasnorrby-dev"
@@ -47,37 +47,36 @@ while getopts f:a:c:dh opt; do
   esac
 done
 
-if [ ! -f .travis.yml ] ; then
+if [ ! -f .travis.yml ]; then
   echo-fail "No travis.yml found in the current directory, aborting"
   exit 1
 fi
 
-if ! command -v travis >/dev/null 2>&1 ; then
+if ! command -v travis >/dev/null 2>&1; then
   echo-fail "Could not find travis cli in path, aborting"
   exit 1
 fi
 
-TOKEN=$(cat $TOKEN_FILE_PATH)
+TOKEN=$(cat "$TOKEN_FILE_PATH")
 
 VALUE=${ACCOUNT}:${TOKEN}
 FIELD=notifications.slack
-if [ ! -z "$CHANNEL" ] ; then
-  VALUE=${VALUE}#${CHANNEL}
+if [ -n "$CHANNEL" ]; then
+  VALUE=${VALUE} #${CHANNEL}
   FIELD=${FIELD}.rooms
 fi
 
-
-if [[ "$OPT_DRY_RUN" == true ]] ; then
+if [[ "$OPT_DRY_RUN" == true ]]; then
   echo-info "Resulting command: travis encrypt --pro $VALUE --add $FIELD"
 else
-  if grep "^notifications" .travis.yml > /dev/null 2>&1 ; then
+  if grep "^notifications" .travis.yml >/dev/null  2>&1; then
     echo-fail ".travis.yml already has a 'notifications' field."
     echo-fail "You should comment or remove it."
     exit 1
   fi
 
   TMPFILE=$(mktemp)
-  cp .travis.yml $TMPFILE
+  cp .travis.yml "$TMPFILE"
 
   echo-info "Logging in to travis..."
   travis login --pro --auto
@@ -87,9 +86,9 @@ else
   LINE_NR=$(grep "^notifications" .travis.yml -n | cut -d ":" -f1)
 
   NOTIFICATION_LINES=$(sed -n "$LINE_NR,\$p" .travis.yml)
-  mv $TMPFILE .travis.yml
+  mv "$TMPFILE" .travis.yml
   echo-info "Adding template settings"
-  cat << EOF >> .travis.yml
+  cat <<EOF  >>.travis.yml
 
 $NOTIFICATION_LINES
     template:
--- scripts/echos.sh
+++ scripts/echos.sh
@@ -2,16 +2,16 @@
 
 # Source this file to use its functions
 
-DIR=$(dirname $([ -L "$0" ] && readlink -f "$0" || echo $0))
-[ -f $DIR/colors.sh ] && . $DIR/colors.sh || (echo "Could not source 'colors', aborting" && exit 1)
+DIR=$(dirname "$([ -L "$0" ] && readlink -f "$0" || echo "$0")")
+[ -f "$DIR/colors.sh" ] && . $DIR/colors.sh || (echo "Could not source 'colors', aborting" && exit 1)
 
-function echo-info  { printf "\r${BG_BLUE}${BLACK}${BOLD} INFO ${NC} %s\n" "$*"; }
-function echo-skip  { printf "\r${BG_DARK_GRAY}${WHITE} SKIP ${NC} %s\n" "$*"; }
-function echo-warn  { printf "\r${BG_ORANGE}${BLACK}${BOLD} WARN ${NC} %s\n" "$*"; }
-function echo-ok  { printf "\r${BG_GREEN}${BLACK}${BOLD}  OK  ${NC} %s\n" "$*"; }
-function echo-fail  { printf "\r${BG_RED}${BLACK}${BOLD} FAIL ${NC} %s\n" "$*"; }
+function echo-info() { printf "\r${BG_BLUE}${BLACK}${BOLD} INFO ${NC} %s\n" "$*"; }
+function echo-skip() { printf "\r${BG_DARK_GRAY}${WHITE} SKIP ${NC} %s\n" "$*"; }
+function echo-warn() { printf "\r${BG_ORANGE}${BLACK}${BOLD} WARN ${NC} %s\n" "$*"; }
+function echo-ok() { printf "\r${BG_GREEN}${BLACK}${BOLD}  OK  ${NC} %s\n" "$*"; }
+function echo-fail() { printf "\r${BG_RED}${BLACK}${BOLD} FAIL ${NC} %s\n" "$*"; }
 
-function echo-input  { printf "\r${BG_ORANGE}${BLACK}${BOLD} INPT ${NC} "; }
+function echo-input() { print "\r${BG_ORANGE}${BLACK}${BOLD} INPT ${NC} "; }
 
 # function echo-info  { printf "\r\033[2K\033[0;34m[ .. ]\033[0m %s\n" "$*"; }
 # function echo-skip  { printf "\r\033[2K\033[38;05;240m[SKIP]\033[0m %s\n" "$*"; }
