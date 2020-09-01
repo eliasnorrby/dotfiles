6c69ca1ff587a1f58cc7f98fa0e5a23db1eb6fb7
Elias Norrby <elias.norrby@gmail.com>
2020-09-01 13:58:17 +0200

    fix(scripts): use printf
    print is a built-in in zsh, but not in bash.

--- scripts/echos.sh
+++ scripts/echos.sh
@@ -11,7 +11,7 @@ function echo-warn() { printf "\r${BG_ORANGE}${BLACK}${BOLD} WARN ${NC} %s\n" "$
 function echo-ok() { printf "\r${BG_GREEN}${BLACK}${BOLD}  OK  ${NC} %s\n" "$*"; }
 function echo-fail() { printf "\r${BG_RED}${BLACK}${BOLD} FAIL ${NC} %s\n" "$*"; }
 
-function echo-input() { print "\r${BG_ORANGE}${BLACK}${BOLD} INPT ${NC} "; }
+function echo-input() { printf "\r${BG_ORANGE}${BLACK}${BOLD} INPT ${NC} "; }
 
 # function echo-info  { printf "\r\033[2K\033[0;34m[ .. ]\033[0m %s\n" "$*"; }
 # function echo-skip  { printf "\r\033[2K\033[38;05;240m[SKIP]\033[0m %s\n" "$*"; }
