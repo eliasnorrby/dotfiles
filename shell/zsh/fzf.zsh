b91bb0230827ac98ca1e6db7e41f3f784ea4be25
Elias Norrby <elias.norrby@gmail.com>
2020-11-20 00:37:00 +0100

    feat(fzf): add pf alias for fuzzy pacman search

--- shell/zsh/fzf.zsh
+++ shell/zsh/fzf.zsh
@@ -274,3 +274,8 @@ p() {
     cd "${HOME}/${TARGET}"
   fi
 }
+
+if [[ "$(_os)" == "arch" ]] ; then
+  # FZF Pacman search
+  alias pf="pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
+fi
