6cfba4d7adbfb6396a31afc220ccfca1880ccffd
Elias Norrby <elias.norrby@gmail.com>
2020-11-23 22:26:48 +0100

    feat(feh): create topic config
    Look for wallpapers in ~, fall back to .dotfiles, and otherwise abort.

---
+++ wm/feh/set-background.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+MAIN="${HOME}/wallpapers"
+FALLBACK="${HOME}/.dotfiles/assets/wallpapers"
+
+if [ -d "$MAIN" ]; then
+  WALLDIR="$MAIN"
+elif [ -d "$FALLBACK" ]; then
+  WALLDIR="$FALLBACK"
+else
+  echo "No wallpapers found"
+  exit 1
+fi
+
+WALL="$(find "$WALLDIR" -type f | sort -R | tail -1)"
+
+[ -n "$WALL" ] && feh --no-fehbg --bg-fill "$WALL"
