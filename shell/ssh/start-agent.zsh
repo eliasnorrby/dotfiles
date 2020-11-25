b57b6585ac2997cc80bdb9a12bcfe0da2cea0b18
Elias Norrby <elias.norrby@gmail.com>
2020-11-25 15:03:23 +0100

    feat(ssh): add topic config

---
+++ shell/ssh/start-agent.zsh
@@ -0,0 +1,7 @@
+# add keys to ssh agent
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+fi
+if [[ ! "$SSH_AUTH_SOCK" ]]; then
+    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+fi
