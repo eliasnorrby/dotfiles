af79d33a4b2f4d4ba2042beda0cbc3907f294b68
Elias Norrby <elias.norrby@gmail.com>
2020-11-26 03:17:53 +0100

    feat(ssh): remove lifetime of unlocked keys
    That is, make it the default: forever.

--- shell/ssh/start-agent.zsh
+++ shell/ssh/start-agent.zsh
@@ -1,6 +1,6 @@
 # add keys to ssh agent
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+    ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
 fi
 if [[ ! "$SSH_AUTH_SOCK" ]]; then
     source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
