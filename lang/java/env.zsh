6de7264bca848735ac6157a72163a1e8770c51ce
Elias Norrby <elias.norrby@gmail.com>
2021-01-21 10:53:28 +0100

    feat(java): rework JAVA_HOME env
    OpenJDK seems to not use a version in the path to its JVM any more. Yay!

--- lang/java/env.zsh
+++ lang/java/env.zsh
@@ -3,12 +3,12 @@ if [[ "$(_os)" == "macos" ]]; then
   # export JAVA_8_HOME="$(/usr/libexec/java_home -v 1.8)"
   export JAVA_8_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home"
   # export JAVA_14_HOME="$(/usr/libexec/java_home -v 14)"
-  export JAVA_14_HOME="/Library/Java/JavaVirtualMachines/openjdk-14.0.2.jdk/Contents/Home"
+  export JAVA_LATEST_HOME="/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home"
 fi
 
 alias java8='export JAVA_HOME=$JAVA_8_HOME'
-alias java14='export JAVA_HOME=$JAVA_14_HOME'
+alias javalatest='export JAVA_HOME=$JAVA_LATEST_HOME'
 
 # default to Java 14
-java14
+javalatest
 # java8
