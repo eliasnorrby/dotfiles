2f0e0d0d7d4567b9a2f1d7a91713928f570b856a
Elias Norrby <elias.norrby@gmail.com>
2021-02-26 11:27:11 +0100

    fix(fzf): change binding for sort toggle
    CTRL-S is taken by session switch.

--- shell/zsh/fzf.zsh
+++ shell/zsh/fzf.zsh
@@ -193,9 +193,9 @@ gg() {
   is_in_git_repo || return
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
   fzf-down --ansi --no-sort --reverse --multi \
-    --bind 'ctrl-s:toggle-sort' \
+    --bind 'ctrl-n:toggle-sort' \
     --bind "ctrl-y:execute:$_gitLogLineToHash | pbcopy" \
-    --header 'Press CTRL-S to toggle sort, CTRL-Y to yank' \
+    --header 'Press CTRL-N to toggle sort, CTRL-Y to yank' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -200' |
   grep -o "[a-f0-9]\{7,\}"
 }
