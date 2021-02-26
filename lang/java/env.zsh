3b4578402292d8592562a7aafc5dd360e0624ae8
Elias Norrby <elias.norrby@gmail.com>
2021-02-26 11:22:12 +0100

    deps(java): add Java 11

--- lang/java/env.zsh
+++ lang/java/env.zsh
@@ -2,11 +2,12 @@ if [[ "$(_os)" == "macos" ]]; then
   # subshell-elimination: uncomment in case of issues with the static locations
   # export JAVA_8_HOME="$(/usr/libexec/java_home -v 1.8)"
   export JAVA_8_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home"
-  # export JAVA_14_HOME="$(/usr/libexec/java_home -v 14)"
+  export JAVA_11_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home"
   export JAVA_LATEST_HOME="/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home"
 fi
 
 alias java8='export JAVA_HOME=$JAVA_8_HOME'
+alias java11='export JAVA_HOME=$JAVA_11_HOME'
 alias javalatest='export JAVA_HOME=$JAVA_LATEST_HOME'
 
 # default to Java 14
