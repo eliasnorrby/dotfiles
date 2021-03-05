ae66e512913536e634ffa76995c208d070b2fb89
Elias Norrby <elias.norrby@gmail.com>
2021-03-05 11:38:14 +0100

    fix(gcloud): eliminate subshells from env.zsh

--- dev/gcloud/env.zsh
+++ dev/gcloud/env.zsh
@@ -1,6 +1,7 @@
 if [[ "$(_os)" == "macos" ]]; then
-  source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-  source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+  # subshell-elimination: use $(brew --prefix) in place of /usr/local in case of change
+  source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+  source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 elif [[ "$(_os)" == "arch" ]]; then
   source /opt/google-cloud-sdk/completion.zsh.inc
 fi
