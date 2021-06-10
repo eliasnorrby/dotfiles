872f60fc9cf24e3f5963361c69f83f6696a90494
Elias Norrby <elias.norrby@gmail.com>
2021-06-11 01:17:00 +0200

    feat(node): lazy-load nvm
    nvm gives a bunch of warnings about an incompatible prefix, but I'm not
    ready to let that go. Instead of relying on nvm for node, I'll use the
    version supplied by nodejs most of the time, falling back to nvm only
    when I need an older version.

--- lang/node/env.zsh
+++ lang/node/env.zsh
@@ -5,3 +5,16 @@ path=("$NPM_PACKAGES/bin" $path)
 # Preserve MANPATH if you already defined it somewhere in your config.
 # Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
 export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
+
+if [ "$(_os)" = "arch" ]; then
+  nvm() {
+
+    unfunction "$0"
+
+    [ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+    source /usr/share/nvm/nvm.sh
+    source /usr/share/nvm/install-nvm-exec
+
+    $0 "$@"
+  }
+fi
