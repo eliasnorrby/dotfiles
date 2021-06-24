2ccb7f7dbe03d8a160975d39a850651e12bd38ea
Elias Norrby <elias.norrby@gmail.com>
2021-06-24 21:14:53 +0200

    refactor(zsh): move alias expansion to separate file
    Also attempt to improve expand-alias-space functionality. Rework the
    "array contains" check to match on the last word in LBUFFER. This means
    ignored aliases won't be expanded if they're used in the middle of a
    line rather than the beginning.

---
+++ shell/zsh/alias_expansion.zsh
@@ -0,0 +1,44 @@
+# alias expansion: https://blog.sebastian-daschner.com/entries/zsh-aliases
+
+# blank aliases - don't add a space after expanding these
+typeset -ga baliases
+baliases=()
+
+balias() {
+  alias $@
+  args="$@"
+  args=${args%%\=*}
+  baliases+=(${args##* })
+}
+
+# ignored aliases - don't expand these
+typeset -ga ialiases
+export ialiases=()
+
+ialias() {
+  alias $@
+  args="$@"
+  args=${args%%\=*}
+  ialiases+=(${args##* })
+}
+
+# functionality
+expand-alias-space() {
+  local remove_space
+  if (($baliases[(Ie)${LBUFFER##* }])); then
+    remove_space="yes"
+  fi
+  if ! (($ialiases[(Ie)${LBUFFER##* }])); then
+    zle _expand_alias
+  fi
+  zle self-insert
+  if [[ -n "$remove_space" ]]; then
+    zle backward-delete-char
+  fi
+}
+zle -N expand-alias-space
+
+bindkey " " expand-alias-space
+# bindkey -M isearch " " magic-space
+
+
--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -1,48 +1,5 @@
 alias rl="exec zsh"
 
-# alias expansion: https://blog.sebastian-daschner.com/entries/zsh-aliases
-
-# blank aliases - don't add a space after expanding these
-typeset -ga baliases
-baliases=()
-
-balias() {
-  alias $@
-  args="$@"
-  args=${args%%\=*}
-  baliases+=(${args##* })
-}
-
-# ignored aliases - don't expand these
-typeset -ga ialiases
-export ialiases=()
-
-ialias() {
-  alias $@
-  args="$@"
-  args=${args%%\=*}
-  ialiases+=(${args##* })
-}
-
-# functionality
-expand-alias-space() {
-  local remove_space
-  if [[ $LBUFFER =~ "\<(${(j:|:)baliases})\$" ]]; then
-    remove_space="yes"
-  fi
-  if [[ ! $LBUFFER =~ "\<(${(j:|:)ialiases})\$" ]]; then
-    zle _expand_alias
-  fi
-  zle self-insert
-  if [[ -n "$remove_space" ]]; then
-    zle backward-delete-char
-  fi
-}
-zle -N expand-alias-space
-
-bindkey " " expand-alias-space
-# bindkey -M isearch " " magic-space
-
 balias clh="curl localhost:"
 
 # global aliases
