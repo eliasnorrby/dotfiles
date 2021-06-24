6f11b11053d5011cf35fc40d007c97d2edc4b141
Elias Norrby <elias.norrby@gmail.com>
2021-06-24 19:23:17 +0200

    feat(zsh): add notify_me with global alias
    Follow a long running command with 'N<space>' to be notified on
    completion.

--- shell/zsh/utilities.zsh
+++ shell/zsh/utilities.zsh
@@ -28,3 +28,24 @@ function list_colors_short() {
 function list_colors_long() {
   for code ({000..255}) print -P -- "$code: %F{$code}This is how your text would look like%f";
 }
+
+BELL_CHARACTER=$(tput bel)
+bell() {
+  printf "%s" "$BELL_CHARACTER"
+}
+
+if [[ "$(_os)" == arch ]]; then
+  notify_me() {
+    notify-send "Done!" "Command execution complete"
+    bell
+  }
+elif [[ "$(_os)" == macos ]]; then
+  notify_me() {
+    terminal-notifier -title "Done!" -message "Command execution complete"
+    bell
+  }
+fi
+
+alias n="notify_me"
+
+alias -g N="; notify_me"
