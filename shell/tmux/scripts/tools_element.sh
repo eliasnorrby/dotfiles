eb360196181b9c141515d45ecf469e4d9936025f
Elias Norrby <elias.norrby@gmail.com>
2021-06-26 12:10:24 +0200

    feat(tmux): add tools status element
    For displaying things like the current kubectl context and tool
    versions. The proper place for things like node and java versions may be
    the prompt - different windows and panes can use different versions. But
    it works for global state.

---
+++ shell/tmux/scripts/tools_element.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+is_callable() {
+  command -v "$1" >/dev/null
+}
+
+kubectl_context() {
+  is_callable kubectl || return
+  context=$(kubectl config current-context)
+  format="#[fg=cyan]☸️ $context"
+  echo "$format"
+}
+
+node_version() {
+  is_callable node || return
+  version=$(node --version | cut -d '.' -f 1)
+  format="#[fg=green] $version"
+  echo "$format"
+}
+
+java_version() {
+  is_callable java || return
+  version=$(java --version | head -1 | cut -d ' ' -f 2 | cut -d '.' -f 1)
+  format="#[fg=gray] $version"
+  echo "$format"
+}
+
+echo "$(kubectl_context) $(node_version) $(java_version)"
