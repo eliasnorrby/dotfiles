b6e01b27865fb20ad3ad5f9ceb1b59ac10c88d5c
Elias Norrby <elias.norrby@gmail.com>
2021-06-26 12:41:06 +0200

    refactor(zsh): clean up funciton names

--- shell/zsh/utilities.zsh
+++ shell/zsh/utilities.zsh
@@ -12,20 +12,20 @@ else
   }
 fi
 
-function mkd() {
+mkd() {
   mkdir -p -- "$1" &&
     cd -P -- "$1"
 }
 
-function color() {
+color() {
   print -P -- "$1: %F{$1}This is what your text would look like%f";
 }
 
-function list_colors_short() {
+list_colors_short() {
   for code ({00..15}) print -P -- "$code: %F{$code}This is what your text would look like%f";
 }
 
-function list_colors_long() {
+list_colors_long() {
   for code ({000..255}) print -P -- "$code: %F{$code}This is how your text would look like%f";
 }
 
