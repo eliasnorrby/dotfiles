b06332f8d4ecf8bb7cfd5bf64e2f54681b2483c3
Elias Norrby <elias.norrby@gmail.com>
2021-07-07 12:53:41 +0200

    feat(kubernetes): add alias for context switching

--- dev/kubernetes/aliases.zsh
+++ dev/kubernetes/aliases.zsh
@@ -2,3 +2,4 @@ alias k="kubectl"
 
 alias ksn="kubectl config set-context --current --namespace"
 alias krn="kubectl config set-context --current --namespace=''"
+alias kuc="kubectl config use-context"
