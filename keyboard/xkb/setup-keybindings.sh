72caba23229cc677a12f9795b9979ec96e12b303
Elias Norrby <elias.norrby@gmail.com>
2021-07-18 00:12:28 +0200

    feat(xkb): add workman layout & refactor xkb
    Rework how keyboard layouts are set. Instead of setting options with
    setxkbmap and piping the output of setxkbmap -print to xkbcomp, call
    xkbcomp directly, pointing to a dedicated config file. The hyper key
    customizations live (mostly) in layout.xkb now, together with the
    layouts.
    
    Furthermore, use ctrl:nocaps instead of capslock:ctrl_modifier.
    
    But most importantly: add the Workman-P layout, slight modifications.
    I'm not ready to replace my Caps Lock / Escape binding for another
    backspace, and I need to ignore the ralt_switch to get the hyper key to
    work.
    
    Modifying the xmodmap call with repeated Hyper_L's seems to be required
    for making the binding work in all layouts.
    
    Weirdly enough, sxhkd does not seem to react to layouts being swapped
    (no matter whether it's launched with the -m -1 option). My hyper
    bindings still work as if I'm using a qwerty layout. Initially, this may
    be helpful - I can concentrate on learning to type with workman, while
    using familiar navigation, but in the long run, it'll just troublesome.
    
    I tried (and failed) to modify the evdev.xml and evdev.lst files in
    /usr/share/X11/xkb to get the polybar xkeyboard module to show different
    names for the different variants. As of now, it shows 'English (US)' for
    both. There's an unreleased %variant% token I can use, hopefully when
    version 3.6.0 is released.
    
    Resources:
    - http://linux.lsdev.sil.org/wiki/index.php/Building_an_XKB_Keyboard
    - https://github.com/workman-layout/Workman/tree/master/xorg
    - https://hack.org/mc/writings/xkb.html
    - https://wiki.archlinux.org/title/X_keyboard_extension

--- keyboard/xkb/setup-keybindings.sh
+++ keyboard/xkb/setup-keybindings.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 
-setxkbmap -layout 'us,se' -option 'grp:shifts_toggle'
-
-# Swap left alt and left super (to mimic Apple keyboard layouts)
-setxkbmap -option 'altwin:swap_lalt_lwin'
-
-# Caps lock works as ctrl when held down
-setxkbmap -option 'caps:ctrl_modifier'
-# Caps lock is Escape
-killall -q xcape
-xcape -e 'Caps_Lock=Escape'
-# So, hold caps lock for ctrl, tap for escape
-
-# Modified version of this suggestion:
-# https://askubuntu.com/a/794087
-setxkbmap -print | \
-  sed -e '/xkb_symbols/s/"[[:space:]]/+local&/' | \
-  xkbcomp -I"${HOME}/.config/xkb" - "${DISPLAY}"
+# Custom layouts defined in xkb/custom
+# - qwerty with hyper key
+# - workman-p with hyper key and default caps lock behaviour
+# Options in xkb/layout.xkb
+# - 'altwin:swap_lalt_lwin': Swap left alt and left super (to mimic Apple keyboard layouts)
+# - 'grp:shifts_toggle': Change layouts with double-shift
+# - 'ctrl:nocaps': Caps lock is Control
+xkbcomp -I"${XDG_CONFIG_HOME}/xkb" "${XDG_CONFIG_HOME}/xkb/layout.xkb" "${DISPLAY}"
 
 # Map right alt to hyper
-xmodmap -e "keycode 108 = Hyper_L"
+xmodmap -e "keycode 108 = Hyper_L Hyper_L Hyper_L Hyper_L"
+
+# Tapping Control is Escape
+killall -q xcape
+xcape
