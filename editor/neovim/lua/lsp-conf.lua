6cdf009a693f7e9696c194b4c92a380e5215845e
Elias Norrby <elias.norrby@gmail.com>
2021-07-19 03:20:22 +0200

    feat(vim): use custom diagnostic signs

--- editor/neovim/lua/lsp-conf.lua
+++ editor/neovim/lua/lsp-conf.lua
@@ -131,3 +131,10 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
         update_in_insert = true
     }
 )
+
+local signs = { Error = "", Warning = "", Hint = "", Information = " " }
+
+for type, icon in pairs(signs) do
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
