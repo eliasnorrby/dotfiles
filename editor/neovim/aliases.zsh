146a9296dfd5017249c290ab4f3a77337caa9afe
Elias Norrby <elias.norrby@gmail.com>
2021-07-19 16:51:42 +0200

    fix(vim): fix vf and vd aliases
    Call Telescope properly.

--- editor/neovim/aliases.zsh
+++ editor/neovim/aliases.zsh
@@ -1,7 +1,7 @@
 alias vim=nvim
 alias v=nvim
-alias vf='nvim +Telescope find_files'
-alias vd="nvim +'cd $DOTFILES' +Telescope find_files"
+alias vf="nvim +'Telescope find_files'"
+alias vd="nvim +'cd $DOTFILES' +'Telescope find_files'"
 alias vp='nvim package.json'
 alias vg='nvim .gitignore'
 alias vlog='nvim +GV'
