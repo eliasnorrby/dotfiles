5a84ab3c83488fb2244c925c00d27521df1f8f5a
Elias Norrby <elias.norrby@gmail.com>
2021-07-19 02:28:33 +0200

    feat(vim): use built-in lsp with compe instead of coc.nvim
    First step of replacing coc.nvim. Other modules (coc-git, coc-explorer,
    etc) to be replaced separately.
    
    Install language servers separately for now - list as dependencies in
    topic.config.yaml. None of the install tools available seem mature
    enough.
    
    Resources:
    - https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
    - https://dev.to/casonadams/neovim-and-its-built-in-language-server-protocol-3j8g

---
+++ editor/neovim/lua/lsp-conf.lua
@@ -0,0 +1,133 @@
+local nvim_lsp = require('lspconfig')
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  -- buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+
+ -- Set some keybinds conditional on server capabilities
+  if client.resolved_capabilities.document_formatting then
+      buf_set_keymap("n", "<leader>ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  elseif client.resolved_capabilities.document_range_formatting then
+      buf_set_keymap("x", "<leader>ff", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
+
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = {
+  "bashls",
+  "diagnosticls",
+  "tsserver",
+  "cssls",
+  "html",
+  "yamlls",
+  "dockerls",
+  "pyright",
+  "jsonls"
+}
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
+
+nvim_lsp.diagnosticls.setup {
+    on_attach = on_attach,
+    -- capabilities = lsp_status.capabilities,
+    cmd = {"diagnostic-languageserver", "--stdio"},
+    filetypes = {
+        "sh",
+        "markdown",
+        "json",
+        "yaml",
+        "toml"
+    },
+    init_options = {
+        linters = {
+            shellcheck = {
+                command = "shellcheck",
+                debounce = 100,
+                args = {"--format", "json", "-"},
+                sourceName = "shellcheck",
+                parseJson = {
+                    line = "line",
+                    column = "column",
+                    endLine = "endLine",
+                    endColumn = "endColumn",
+                    message = "${message} [${code}]",
+                    security = "level"
+                },
+                securities = {
+                    error = "error",
+                    warning = "warning",
+                    info = "info",
+                    style = "hint"
+                }
+            }
+        },
+        filetypes = {
+            sh = "shellcheck"
+        },
+        formatters = {
+            shfmt = {
+                command = "shfmt",
+                args = {"-filename", "script.sh"}
+            },
+            prettier = {
+                command = "prettier",
+                args = {"--stdin-filepath", "%filepath"},
+            }
+        },
+        formatFiletypes = {
+            sh = "shfmt",
+            json = "prettier",
+            yaml = "prettier",
+            toml = "prettier",
+            markdown = "prettier",
+            lua = "prettier"
+        }
+    }
+}
+
+-- Enable diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    {
+        underline = true,
+        virtual_text = false,
+        signs = true,
+        update_in_insert = true
+    }
+)
