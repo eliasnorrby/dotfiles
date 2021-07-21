8b70cfafb2cce6626425f41a1d4b4ec286e79026
Elias Norrby <elias.norrby@gmail.com>
2021-07-21 02:56:06 +0200

    refactor(feh): rewrite choose_background.sh
    Turns out the fuzzy finder interface wasn't as good as simply displaying
    the backgrounds and choosing one using the, *cough*, uhm... arrow keys.

--- wm/feh/choose_background.sh
+++ wm/feh/choose_background.sh
@@ -4,10 +4,8 @@ WALLDIR="${HOME}/wallpapers"
 
 [ ! -d "$WALLDIR" ] && exit 1
 
-background=$(find "$WALLDIR" -type f | fzf --preview 'feh --no-fehbg --bg-fill {}')
-
-if [ -z "$background" ]; then
-  [ -x ~/.fehbg ] && ~/.fehbg
-else
-  feh --bg-fill "$background"
-fi
+feh \
+  --fullscreen \
+  --auto-zoom \
+  --action 'feh --bg-fill %F; xdotool key q' \
+  "$WALLDIR"
