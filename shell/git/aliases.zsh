5fbed06e39d56bc722790f17117d6e9b047f30f2
Elias Norrby <elias.norrby@gmail.com>
2021-10-27 10:07:41 +0200

    feat(git): add lsnotrepos for listing non-repo dirs

--- shell/git/aliases.zsh
+++ shell/git/aliases.zsh
@@ -2,3 +2,7 @@ alias g="git"
 ialias git="nocorrect git"
 
 alias grvw="gh repo view -w"
+
+lsnotrepos() {
+  comm <(find . -maxdepth 1 -type d | sort) <(find . -name .git -maxdepth 2 | xargs dirname | sort) -3
+}
