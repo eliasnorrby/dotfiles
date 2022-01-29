c3e8ee098283fb5587ecdf48d4fd6bfe57d2c3dc
Elias Norrby <elias.norrby@gmail.com>
2022-01-30 00:58:44 +0100

    ci: fix warnings in test scripts

--- _tests/_helpers.sh
+++ _tests/_helpers.sh
@@ -7,14 +7,15 @@ function get_complete_list() {
   [ -z "$1" ] && echo "Must provide a key" && return 1
   local config_key=$1
   local values=""
-  for topic_file in ${topic_config_files[@]}; do
-    local topic_name=${topic_file%/*}
-    local topic_name=${topic_name##*/}
-    local topic_values=$(yq eval ".${topic_name}_config.${config_key}" $topic_file -j | jq -r '.[]?')
+  for topic_file in "${topic_config_files[@]}"; do
+    local topic_name topic_values
+    topic_name=${topic_file%/*}
+    topic_name=${topic_name##*/}
+    topic_values=$(yq eval ".${topic_name}_config.${config_key}" "$topic_file" -o=json | jq -r '.[]?')
     values="$values $topic_values"
   done
 
-  values=$(echo $values | tr ' ' '\n' | sort | uniq | tr '\n' ' ')
+  values=$(echo "$values" | tr ' ' '\n' | sort | uniq | tr '\n' ' ')
 
-  echo $values
+  echo "$values"
 }
--- _tests/packages_test.sh
+++ _tests/packages_test.sh
@@ -5,7 +5,7 @@ echo "Checking to see if all listed npm packages are available"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 echo "DIR: $DIR"
-. $DIR/_helpers.sh
+. "$DIR/_helpers.sh"
 
 npm_packages=$(get_complete_list "npm_packages")
 
@@ -15,12 +15,11 @@ echo
 
 echo "> Checking npm packages..."
 for package in $npm_packages; do
-  printf "${package}... "
-  npm info $package > /dev/null
-  [ $? -eq 0 ] && printf " OK \n" || should_fail=true
+  printf "%s... " "${package}"
+  npm info "$package" > /dev/null && printf " OK \n" || should_fail=true
 done
 
-ELAPSED="$(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+ELAPSED="$((SECONDS / 3600))hrs $(((SECONDS / 60) % 60))min $((SECONDS % 60))sec"
 echo "Check completed in $ELAPSED"
 
 if [[ "$should_fail" == true ]] ; then
