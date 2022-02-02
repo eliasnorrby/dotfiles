6d7b84bdfe302860a310da18f6fe3bc1d664f987
Elias Norrby <elias.norrby@gmail.com>
2022-02-02 10:33:45 +0100

    conf(vim): replace lsp mappings with telescope pickers

--- editor/neovim/lua/telescope-conf.lua
+++ editor/neovim/lua/telescope-conf.lua
@@ -10,3 +10,19 @@ require('telescope').setup{
   }
 }
 require('telescope').load_extension('fzf')
+
+local opts = {noremap=true, silent=true }
+local function lsp_map(mode, keybind, command)
+  vim.api.nvim_set_keymap(
+    mode,
+    keybind,
+    "<cmd>lua require'telescope.builtin'." .. command .. "()<CR>",
+    opts
+  )
+end
+
+lsp_map('n', 'gr', "lsp_references")
+lsp_map('n', '<leader>ca', "lsp_code_actions")
+lsp_map('n', 'gd', "lsp_definitions")
+lsp_map('n', 'gi', "lsp_implementations")
+lsp_map('n', 'go', "lsp_document_symbols")
