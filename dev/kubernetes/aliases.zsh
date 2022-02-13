f40c860006698d6f461853f7d2160c6859a9935a
Elias Norrby <elias.norrby@gmail.com>
2022-02-14 00:03:35 +0100

    feat(kubernetes): add aliases for delete

--- dev/kubernetes/aliases.zsh
+++ dev/kubernetes/aliases.zsh
@@ -5,7 +5,9 @@ alias krn="kubectl config set-context --current --namespace=''"
 alias kuc="kubectl config use-context"
 
 alias kaf="kubectl apply -f"
+alias kdf="kubectl delete -f"
 alias kak="kubectl apply -k"
+alias kdk="kubectl delete -k"
 alias kcf="kubectl create -f"
 
 alias h="helm"
