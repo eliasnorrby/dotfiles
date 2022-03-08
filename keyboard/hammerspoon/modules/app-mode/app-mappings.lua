8b9380ef152a54e9be598a1657c51bc074c0b969
Elias Norrby <elias.norrby@gmail.com>
2022-03-08 12:25:08 +0100

    conf(hammerspoon): bind kitty to T and alacritty to R

--- keyboard/hammerspoon/modules/app-mode/app-mappings.lua
+++ keyboard/hammerspoon/modules/app-mode/app-mappings.lua
@@ -7,13 +7,13 @@ return {
   { 'f', 'Fantastical 2' },        -- "F" for "Fantastical"
   { 's', 'Emacs' },                -- "S" for "Sublime"
   { 'g', 'Todoist' },              -- "G" for "GTD"
-  { 't', 'Alacritty' },            -- "T" for "Terminal"
+  { 't', 'kitty' },            -- "T" for "Terminal"
   { 'd', 'Drafts' },               -- "D" for "Drafts"
   { 'm', 'Gmail'},                 -- "M" for "Mail"
   { 'q', 'MySQLWorkbench'},        -- "Q" for "sQl"
   { 'n', 'Postman'},
   { 'i', 'Insomnia'},              -- "I" for "Insomnia"
-  { 'r', 'kitty' },
+  { 'r', 'Alacritty' },
   { 'u', 'Spotify' },              -- "U" for some reason
   { 'x', 'Firefox Developer Edition' },
   { 'w', 'Microsoft PowerPoint' },
