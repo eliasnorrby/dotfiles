5cfd0e40069841d9d85aea689fbfc271a3dbf306
Elias Norrby <elias.norrby@gmail.com>
2022-04-07 01:03:40 +0200

    feat(git): add list_branch_files.sh

---
+++ shell/git/list_branch_files.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+usage() {
+  cat <<-EOF >&2
+	${0##*/} <list|diff>
+
+	List files changed on current branch, compared to the base branch. The base
+	branch is guessed to be 'main' or 'master'. If none of them is found, lists
+	changed files relative to HEAD instead.
+	EOF
+}
+
+main() {
+  local base_branch cmd action
+  action=$(parse_args "$@")
+  base_branch=$(guess_base_branch)
+  cmd=(git)
+  if [[ "$action" == "diff" ]]; then
+    cmd+=(--no-pager)
+  fi
+  cmd+=(diff --diff-filter=ACMR)
+  if [[ -n "$base_branch" ]]; then
+    cmd+=(--relative "$base_branch")
+  fi
+  case $action in
+    list)
+      cmd+=(--name-only)
+      "${cmd[@]}"
+      ;;
+    diff)
+      "${cmd[@]}" "$2"
+      ;;
+  esac
+}
+
+parse_args() {
+  case "$1" in
+    "") echo "list" ;;
+    list) echo "list" ;;
+    diff) echo "diff" ;;
+    -h | --help)
+      usage
+      exit
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+}
+
+guess_base_branch() {
+  for branch in main master; do
+    if is_branch "$branch"; then
+      echo "$branch"
+      return
+    fi
+  done
+}
+
+is_branch() {
+  git rev-parse --verify "$1" >/dev/null 2>&1
+}
+
+main "$@"
