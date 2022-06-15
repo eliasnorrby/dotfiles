d4aed78a7dca2b041e531fd09b61941493385e40
Elias Norrby <elias.norrby@gmail.com>
2022-06-15 15:16:22 +0200

    conf(fzf): increase range of fuzzy args

--- shell/zsh/fzf.zsh
+++ shell/zsh/fzf.zsh
@@ -88,7 +88,7 @@ fgr() {
 # select from previous long-ish arguments
 fargs() {
   local args
-  history -50 | cut -c 8- | sed 's/ /\n/g' | sed '/^.\{0,5\}$/d' | sort | uniq | fzf_tmux --reverse
+  history -500 | cut -c 8- | sed 's/ /\n/g' | sed '/^.\{0,5\}$/d' | sort | uniq | fzf_tmux --reverse
 }
 
 fzf-args-widget() { local result=$(fargs | join-lines); zle reset-prompt; LBUFFER+=$result }
