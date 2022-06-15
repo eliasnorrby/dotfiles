47f7e1b24c82ba5f1b6da6051a81e28f66898bcb
Elias Norrby <elias.norrby@gmail.com>
2022-06-15 22:22:29 +0200

    refactor(zsh): rename projects helper

--- shell/zsh/fzf.zsh
+++ shell/zsh/fzf.zsh
@@ -36,7 +36,7 @@ export FZF_TMUX_OPTS='-p 70%,70%'
 alias j="_z 2>&1"
 unalias z 2> /dev/null
 z() {
-  p "$@"
+  projects "$@"
   return
   [ $# -gt 0 ] && _z "$*" && return
   cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
@@ -253,7 +253,7 @@ fi
 
 # FZF Project search
 
-p() {
+projects() {
   local query=$1
   local DIR_LIST=(
     "${HOME}/dev"
