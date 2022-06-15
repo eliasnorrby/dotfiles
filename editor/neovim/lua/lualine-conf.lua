ce9259c2167ebc6abc22572367df612f6fe6e2e3
Elias Norrby <elias.norrby@gmail.com>
2022-06-15 15:11:51 +0200

    feat(vim): add argument list indicator to statusline

--- editor/neovim/lua/lualine-conf.lua
+++ editor/neovim/lua/lualine-conf.lua
@@ -28,7 +28,7 @@ require'lualine'.setup {
     lualine_b = {'branch'},
     lualine_c = {'filename'},
     -- lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_x = { diagnostics, 'filetype' },
+    lualine_x = { '%a', diagnostics, 'filetype' },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
