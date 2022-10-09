2a0634c6b54b42ff16c0d98c861e106481df1db9
Elias Norrby <elias.norrby@gmail.com>
2022-10-09 12:44:37 +0200

    refactor(vim): remove use of deprecated setting

--- editor/neovim/lua/lsp-conf.lua
+++ editor/neovim/lua/lsp-conf.lua
@@ -35,9 +35,9 @@ local on_attach = function(client, bufnr)
 
 
  -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
+  if client.server_capabilities.document_formatting then
       buf_set_keymap("n", "<leader>fF", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
+  elseif client.server_capabilities.document_range_formatting then
       buf_set_keymap("x", "<leader>fF", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
