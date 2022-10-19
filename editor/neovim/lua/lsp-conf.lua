c8aebe98ba599c907b5af3be29cbab009c518fd5
Elias Norrby <elias.norrby@gmail.com>
2022-10-19 11:53:17 +0200

    fix(vim): fix broken diagnostics icons

--- editor/neovim/lua/lsp-conf.lua
+++ editor/neovim/lua/lsp-conf.lua
@@ -186,6 +186,6 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 local signs = { Error = "", Warning = "", Hint = "", Information = " " }
 
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
+  local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
