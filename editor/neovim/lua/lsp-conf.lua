34aba1de4b03f18954dcf0e5857137793b143904
Elias Norrby <elias.norrby@gmail.com>
2022-10-19 12:39:04 +0200

    fix(vim): update cmp config

--- editor/neovim/lua/lsp-conf.lua
+++ editor/neovim/lua/lsp-conf.lua
@@ -39,7 +39,7 @@ local lsp_defaults = {
   flags = {
     debounce_text_changes = 150,
   },
-  capabilities = require('cmp_nvim_lsp').update_capabilities(
+  capabilities = require('cmp_nvim_lsp').default_capabilities(
     vim.lsp.protocol.make_client_capabilities()
   ),
 }
