ff8d5af08880f76e9bae2fa84aea1b95dd671caf
Elias Norrby <elias.norrby@gmail.com>
2022-10-19 11:52:30 +0200

    feat(vim): remap quickfix lsp references

--- editor/neovim/lua/lsp-conf.lua
+++ editor/neovim/lua/lsp-conf.lua
@@ -30,7 +30,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', '<space>,', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', 'glr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<space>fF', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
