df8d3df8415175ca073d6fd974eb1ca7f1556c07
Elias Norrby <elias.norrby@gmail.com>
2022-11-28 16:01:31 +0100

    feat(vim): add telescope etc

--- editor/neovim/lua/packer_init.lua
+++ editor/neovim/lua/packer_init.lua
@@ -14,6 +14,11 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
   use 'kyazdani42/nvim-tree.lua'
 
   use 'marko-cerovac/material.nvim'
