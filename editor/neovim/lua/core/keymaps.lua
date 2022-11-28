a385bfd034b70b724f04624bc5743e5ccb58df35
Elias Norrby <elias.norrby@gmail.com>
2022-11-28 15:26:07 +0100

    refactor(vim): use init.lua

---
+++ editor/neovim/lua/core/keymaps.lua
@@ -0,0 +1,16 @@
+-----------------------------------------------------------
+-- Define keymaps of Neovim and installed plugins.
+-----------------------------------------------------------
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap=true, silent=true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Change leader to space
+vim.g.mapleader = ' '
+
+map('n', '<leader>q', ':q<CR>')
---
+++ editor/neovim/lua/core/options.lua
@@ -0,0 +1,47 @@
+-----------------------------------------------------------
+-- General
+-----------------------------------------------------------
+
+local g = vim.g       -- Global variables
+local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
+
+opt.shortmess:append "sI"
+
+-----------------------------------------------------------
+-- Startup
+-----------------------------------------------------------
+-- Disable nvim intro
+opt.shortmess:append "sI"
+
+-- Disable builtin plugins
+local disabled_built_ins = {
+   "2html_plugin",
+   "getscript",
+   "getscriptPlugin",
+   "gzip",
+   "logipat",
+   "netrw",
+   "netrwPlugin",
+   "netrwSettings",
+   "netrwFileHandlers",
+   "matchit",
+   "tar",
+   "tarPlugin",
+   "rrhelper",
+   "spellfile_plugin",
+   "vimball",
+   "vimballPlugin",
+   "zip",
+   "zipPlugin",
+   "tutor",
+   "rplugin",
+   "synmenu",
+   "optwin",
+   "compiler",
+   "bugreport",
+   "ftplugin",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+   g["loaded_" .. plugin] = 1
+end
