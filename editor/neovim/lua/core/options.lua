420f15adafbbf83d068758c7538746ddfefa2f3f
Elias Norrby <elias.norrby@gmail.com>
2022-11-28 16:12:51 +0100

    feat(vim): set options

--- editor/neovim/lua/core/options.lua
+++ editor/neovim/lua/core/options.lua
@@ -11,6 +11,29 @@ opt.path:append("**")
 
 opt.clipboard = 'unnamedplus'
 
+opt.swapfile = false
+opt.backup = false
+
+opt.tabstop = 8
+opt.softtabstop = 2
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.autoindent = true
+opt.linebreak = true
+
+opt.splitbelow = true
+opt.splitright = true
+opt.scrolloff = 5
+opt.showcmd = false
+opt.ruler = false
+opt.signcolumn = 'yes'
+
+-----------------------------------------------------------
+-- UI
+-----------------------------------------------------------
+opt.number = true
+opt.relativenumber = true
+
 -----------------------------------------------------------
 -- Startup
 -----------------------------------------------------------
