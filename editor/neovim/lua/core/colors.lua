750a7c21bd39972623800ad087790bd16131dd42
Elias Norrby <elias.norrby@gmail.com>
2022-12-07 08:25:56 +0100

    feat(vim): configure material colorscheme

--- editor/neovim/lua/core/colors.lua
+++ editor/neovim/lua/core/colors.lua
@@ -1,5 +1,25 @@
 vim.g.material_style = 'palenight'
 
-require('material').setup({})
+require('material').setup({
+  contrast = {
+    sidebars = true,
+  },
+  disable = {
+    borders = false,
+  },
+  styles = {
+    comments = { italic = true },
+    functions = { bold = true },
+  },
+  plugins = {
+    'gitsigns',
+    'hop',
+    'nvim-cmp',
+    'nvim-tree',
+    'telescope',
+    'trouble',
+    'which-key',
+  },
+})
 
 vim.cmd 'colorscheme material'
