f9bbdbef938235706953e7ac7b44aa29455245ec
Elias Norrby <elias.norrby@gmail.com>
2022-12-25 00:12:50 +0100

    feat(vim): add tsserver lsp config

--- editor/neovim/after/plugin/lsp.lua
+++ editor/neovim/after/plugin/lsp.lua
@@ -80,6 +80,20 @@ local servers = {
   },
   tailwindcss = {},
   terraformls = {},
+  tsserver = {
+    commands = {
+      OrganizeImports = {
+        function()
+          vim.lsp.buf.execute_command({
+            command = "_typescript.organizeImports",
+            arguments = {vim.api.nvim_buf_get_name(0)},
+            title = ""
+          })
+        end,
+        description = "Organize Imports"
+      }
+    },
+  },
   yamlls = {
     settings = {
       yaml = {
