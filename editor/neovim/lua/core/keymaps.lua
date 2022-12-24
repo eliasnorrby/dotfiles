d431457c26746b99d14d69de8ad9372daafa68e5
Elias Norrby <elias.norrby@gmail.com>
2022-12-24 01:19:11 +0100

    feat(vim): add window map

--- editor/neovim/lua/core/keymaps.lua
+++ editor/neovim/lua/core/keymaps.lua
@@ -1,16 +1,7 @@
------------------------------------------------------------
--- Define keymaps of Neovim and installed plugins.
------------------------------------------------------------
+local u = require('core.utils')
 
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap=true, silent=true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
--- Change leader to space
 vim.g.mapleader = ' '
 
-map('n', '<leader>q', ':q<CR>')
+u.map('n', '<leader>q', ':q<CR>')
+
+u.map('n', '<leader>w', '<C-W>')
