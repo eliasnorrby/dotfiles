bac27c3a8e54761856a9985686c12942e45cdefa
Elias Norrby <elias.norrby@gmail.com>
2022-12-24 01:49:52 +0100

    feat(vim): add telescope bindings

--- editor/neovim/after/plugin/telescope.lua
+++ editor/neovim/after/plugin/telescope.lua
@@ -1,3 +1,29 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+require('telescope').setup{
+  defaults = {
+    prompt_prefix = "λ ",
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close
+      },
+    },
+  }
+}
+
+local project_files = function()
+  local opts = {}
+  vim.fn.system('git rev-parse --is-inside-work-tree')
+  if vim.v.shell_error == 0 then
+    require"telescope.builtin".git_files({ use_git_root=false })
+  else
+    require"telescope.builtin".find_files(opts)
+  end
+end
+
+vim.keymap.set('n', '<leader><leader>', project_files, {})
+vim.keymap.set('n', '<leader>.', builtin.find_files, {})
+vim.keymap.set('n', '<leader><cr>', builtin.git_status, {})
+
+vim.keymap.set('n', '<leader>//', builtin.live_grep, {})
