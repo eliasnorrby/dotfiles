166375a75f28a151f4d3c2bdc41b5ab872262229
Elias Norrby <elias.norrby@gmail.com>
2022-12-25 01:23:57 +0100

    refactor(vim): remove packer autocmd

--- editor/neovim/lua/core/autocommands.lua
+++ editor/neovim/lua/core/autocommands.lua
@@ -1,10 +1,3 @@
-vim.cmd([[
-augroup packer_user_config
-  autocmd!
-  autocmd BufWritePost packer_init.lua source <afile> | PackerCompile
-augroup end
-]])
-
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
