073088f03c92cab41063da25fb153bd4455fc15d
Elias Norrby <elias.norrby@gmail.com>
2022-12-25 21:18:18 +0100

    feat(vim): migrate more keymaps

--- editor/neovim/lua/core/keymaps.lua
+++ editor/neovim/lua/core/keymaps.lua
@@ -25,3 +25,26 @@ u.map('n', '<leader>olo', function()
   vim.wo.number = false
   vim.wo.relativenumber = false
 end)
+
+-- other
+u.map('x', 'K', ":move '<-2<CR>gv=gv")
+u.map('x', 'J', ":move '>+1<CR>gv=gv")
+
+u.map('n', 'Q', '<nop>')
+u.map('n', '-', '<nop>')
+
+u.map('n', '<C-E>', '3<C-E>')
+u.map('n', '<C-Y>', '3<C-Y>')
+
+u.map('n', 'j', 'gj')
+u.map('n', 'k', 'gk')
+
+u.map('n', '<leader>co', vim.cmd.copen)
+u.map('n', '<leader>cc', vim.cmd.cclose)
+
+-- for heredoc formatting
+u.map('x', '<leader>ft', ':s/^  /	/<cr>')
+
+-- temporary
+u.map('n', '<leader>so', vim.cmd.source)
+
