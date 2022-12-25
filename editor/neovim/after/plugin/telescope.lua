2243b417f333be9208211f2bed89b59adcec88ab
Elias Norrby <elias.norrby@gmail.com>
2022-12-25 22:38:02 +0100

    style: format

--- editor/neovim/after/plugin/telescope.lua
+++ editor/neovim/after/plugin/telescope.lua
@@ -2,7 +2,7 @@ local u = require('core.utils')
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 
-require('telescope').setup{
+require('telescope').setup({
   defaults = {
     prompt_prefix = "λ ",
     mappings = {
@@ -11,20 +11,20 @@ require('telescope').setup{
       },
     },
   }
-}
+})
 
 local project_files = function()
   local opts = {}
   vim.fn.system('git rev-parse --is-inside-work-tree')
   if vim.v.shell_error == 0 then
-    require"telescope.builtin".git_files({ use_git_root=false })
+    require("telescope.builtin").git_files({ use_git_root = false })
   else
-    require"telescope.builtin".find_files(opts)
+    require("telescope.builtin").find_files(opts)
   end
 end
 
 u.map('n', '<leader><leader>', project_files, { desc = 'Find project files' })
-u.map('n', '<leader>.', builtin.find_files, { desc = 'Find (non-git) project files'})
+u.map('n', '<leader>.', builtin.find_files, { desc = 'Find (non-git) project files' })
 u.map('n', '<leader><cr>', builtin.git_status, { desc = 'Find changed files' })
 
 u.map('n', '<leader>//', builtin.live_grep, { desc = 'Project grep' })
