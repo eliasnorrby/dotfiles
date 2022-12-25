218c620a720df6d9b4cc2137b3b0668847635bf4
Elias Norrby <elias.norrby@gmail.com>
2022-12-25 01:15:32 +0100

    feat(vim): add cmp window borders

--- editor/neovim/after/plugin/cmp.lua
+++ editor/neovim/after/plugin/cmp.lua
@@ -7,6 +7,13 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   mapping = cmp.mapping.preset.insert {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -39,4 +46,3 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
-
