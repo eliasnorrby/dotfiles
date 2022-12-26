87458ac0366c3eeb862d9c3a63d4540ff5214505
Elias Norrby <elias.norrby@gmail.com>
2022-12-26 11:17:03 +0100

    feat(vim): add formatter.nvim

--- editor/neovim/after/plugin/lsp.lua
+++ editor/neovim/after/plugin/lsp.lua
@@ -30,11 +30,6 @@ local on_attach = function(_, bufnr)
   nmap('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
 end
 
 vim.diagnostic.config({
--- editor/neovim/lua/core/packer.lua
+++ editor/neovim/lua/core/packer.lua
@@ -86,6 +86,8 @@ return require('packer').startup(function(use)
     },
   }
 
+  use 'mhartington/formatter.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
