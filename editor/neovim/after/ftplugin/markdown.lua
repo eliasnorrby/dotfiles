de368385482cab983ea483858577c3f9ad7fa2a8
Elias Norrby <elias.norrby@gmail.com>
2023-01-01 13:11:11 +0100

    feat(vim): add read mode toggle to markdown

--- editor/neovim/after/ftplugin/markdown.lua
+++ editor/neovim/after/ftplugin/markdown.lua
@@ -1,5 +1,4 @@
-vim.opt_local.conceallevel = 2
-vim.opt_local.concealcursor = 'nc'
+vim.opt_local.list = true
 
 vim.keymap.set('n', 'gf', function()
   if require('obsidian').util.cursor_on_markdown_link() then
@@ -8,3 +7,24 @@ vim.keymap.set('n', 'gf', function()
     return 'gf'
   end
 end, { noremap = false, expr = true })
+
+local toggle_read_mode = function()
+  local is_in_read_mode = not vim.opt_local.modifiable:get()
+  if is_in_read_mode then
+    vim.opt_local.number = true
+    vim.opt_local.relativenumber = true
+    vim.opt_local.conceallevel = 0
+    vim.opt_local.concealcursor = ''
+    vim.opt_local.list = true
+    vim.opt_local.modifiable = true
+  else
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.conceallevel = 2
+    vim.opt_local.concealcursor = 'nc'
+    vim.opt_local.list = false
+    vim.opt_local.modifiable = false
+  end
+end
+
+vim.keymap.set('n', '<leader>tr', toggle_read_mode, { noremap = true, silent = true })
