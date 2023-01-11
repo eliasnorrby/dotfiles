04a552e0dc5b3a0b04df9dadc1e0ad06bdae07df
Elias Norrby <elias.norrby@gmail.com>
2023-01-11 10:45:47 +0100

    feat(vim): add code actions , mapping

--- editor/neovim/after/plugin/lsp.lua
+++ editor/neovim/after/plugin/lsp.lua
@@ -11,6 +11,7 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>,', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
