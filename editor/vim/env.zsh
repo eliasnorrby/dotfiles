b8e3cff18057e6b141924210faef86fadaff6eb0
Elias Norrby <elias.norrby@gmail.com>
2023-01-16 13:09:35 +0100

    refactor(vim): remove legacy vim config

--- editor/vim/env.zsh
+++
@@ -1,6 +0,0 @@
-# export VIM_DIR="$HOME/.vim"
-# export VIMINIT="let \$MYVIMRC='$XDG_CONFIG_HOME/vim/.vimrc' | source \$MYVIMRC"
-
-# FIXME: do I use these for vim or nvim?
-# export BAT_THEME=base16
-# export VIM_COLOR=palenight
