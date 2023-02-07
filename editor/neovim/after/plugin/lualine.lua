7c5c1937841fbf93c09d7832e9a77b22ddc121ed
Elias Norrby <elias.norrby@gmail.com>
2023-02-07 09:02:12 +0100

    fix(vim): remove lualine tabline
    It's buggy with more than 2 tabs.

--- editor/neovim/after/plugin/lualine.lua
+++ editor/neovim/after/plugin/lualine.lua
@@ -40,28 +40,5 @@ require('lualine').setup({
     lualine_y = {},
     lualine_z = {},
   },
-  tabline = {
-    lualine_z = {
-      {
-        'tabs',
-        fmt = function(name, context)
-          -- workaround for lualine having hardcoded showtabline = 2
-          if vim.go.showtabline ~= 2 and vim.fn.tabpagenr('$') > 0 then
-            vim.go.showtabline = 2
-          end
-          if vim.go.showtabline == 2 and vim.fn.tabpagenr('$') < 2 then
-            vim.go.showtabline = 1
-          end
-          -- Show + if buffer is modified in tab
-          local buflist = vim.fn.tabpagebuflist(context.tabnr)
-          local winnr = vim.fn.tabpagewinnr(context.tabnr)
-          local bufnr = buflist[winnr]
-          local mod = vim.fn.getbufvar(bufnr, '&mod')
-
-          return name .. (mod == 1 and ' +' or '')
-        end,
-      },
-    },
-  },
   extensions = { 'fugitive', 'nvim-tree', 'quickfix' },
 })
