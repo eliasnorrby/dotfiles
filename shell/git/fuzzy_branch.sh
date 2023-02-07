8d0fce08ea8d4049bbfdd359814a075fb8b9824f
Elias Norrby <elias.norrby@gmail.com>
2023-02-07 09:22:47 +0100

    fix(git): do not list remote branches in fuzzy_branch

--- shell/git/fuzzy_branch.sh
+++ shell/git/fuzzy_branch.sh
@@ -7,7 +7,7 @@ is_in_git_repo() {
 }
 
 fzf_branch_select() {
-  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  git branch --color=always | grep -v '/HEAD\s' | sort |
   fzf --ansi --multi --tac --preview-window right:70% \
     --preview 'git log --graph --color --abbrev-commit --branches --pretty="'"$GIT_LOG_FORMAT"'" $(sed s/^..// <<< {} | cut -d" " -f1) | head -200' |
   sed 's/^..//' | cut -d' ' -f1 |
