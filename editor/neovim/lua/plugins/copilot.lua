d3346f412fa0134c1330d2454cc520506475f23b
Elias Norrby <elias.norrby@gmail.com>
2023-05-30 10:59:58 +0200

    fix(vim): replace copilot binding with ;
    <C-P> conflicts with cmp's completion and snippet binding.

--- editor/neovim/lua/plugins/copilot.lua
+++ editor/neovim/lua/plugins/copilot.lua
@@ -3,6 +3,6 @@ return {
   init = function()
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
-    vim.api.nvim_set_keymap('i', '<C-P>', 'copilot#Accept("<C-P>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap('i', ';', 'copilot#Accept(";")', { silent = true, expr = true })
   end,
 }
