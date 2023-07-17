7778d34967a1b05c41c5b93539775ed516724545
Elias Norrby <elias.norrby@gmail.com>
2023-07-17 10:37:06 +0200

    feat(git): add apb alias

--- shell/git/aliases.zsh
+++ shell/git/aliases.zsh
@@ -2,6 +2,7 @@ alias g="git"
 ialias git="nocorrect git"
 
 alias grvw="gh repo view -w"
+alias abp="auto_branch_pruner"
 
 lsnotrepos() {
   comm <(find . -maxdepth 1 -type d | sort) <(find . -name .git -maxdepth 2 | xargs dirname | sort) -3
