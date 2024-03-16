1df1c1ee3470dcf204dc41272e50fc1ee2bc9295
Elias Norrby <elias.norrby@gmail.com>
2024-03-16 11:12:17 +0100

    feat(zsh): add direnv

--- shell/zsh/plugins.zsh
+++ shell/zsh/plugins.zsh
@@ -1,2 +1,4 @@
 _load_silent shell/zsh/plugins/system-clipboard/zsh-system-clipboard.plugin.zsh
 _load_silent shell/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+
+eval "$(direnv hook zsh)"
