368861ec8cdd2ca4fdb2aed1de31235f949ad218
Elias Norrby <elias.norrby@gmail.com>
2024-04-25 12:02:26 +0200

    refactor(vim): move bbye config

--- editor/neovim/after/plugin/vim-bbye.lua
+++
@@ -1,4 +0,0 @@
-local u = require('core.utils')
-
-u.map('n', '<leader>bk', vim.cmd.Bdelete)
-u.map('n', '<leader>bK', ':Bdelete!<CR>')
--- editor/neovim/lua/plugins/init.lua
+++ editor/neovim/lua/plugins/init.lua
@@ -2,7 +2,21 @@ return {
   'gpanders/editorconfig.nvim',
   'tpope/vim-unimpaired',
   'tpope/vim-abolish',
-  'moll/vim-bbye',
+  {
+    'moll/vim-bbye',
+    keys = {
+      {
+        '<leader>bk',
+        vim.cmd.Bdelete,
+        desc = 'Delete buffer',
+      },
+      {
+        '<leader>bK',
+        ':Bdelete!<CR>',
+        desc = 'Delete buffer (!)',
+      },
+    },
+  },
 
   { 'nvim-tree/nvim-tree.lua', dependencies = { 'nvim-tree/nvim-web-devicons' } },
 
