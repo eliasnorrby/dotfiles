41272f70f922218d951f57c926168f527952c375
Elias Norrby <elias.norrby@gmail.com>
2024-05-27 10:10:27 +0200

    refactor(vim): use lua copilot plugin

--- editor/neovim/lua/plugins/copilot.lua
+++ editor/neovim/lua/plugins/copilot.lua
@@ -1,8 +1,23 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  lazy = true,
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  opts = {
+    suggestion = {
+      auto_trigger = true,
+      keymap = {
+        accept = false,
+      },
+    },
+  },
   init = function()
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
-    vim.api.nvim_set_keymap('i', ';', 'copilot#Accept(";")', { silent = true, expr = true })
+    vim.keymap.set('i', ';', function()
+      if require('copilot.suggestion').is_visible() then
+        require('copilot.suggestion').accept()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(';', true, false, true), 'n', false)
+      end
+    end)
   end,
 }
