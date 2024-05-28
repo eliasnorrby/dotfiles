06b7bf4f710ff29dec3c59c5996c65f17ca3eba1
Elias Norrby <elias.norrby@gmail.com>
2024-05-28 11:18:16 +0200

    chore(zsh): remove folder management

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -39,20 +39,6 @@ alias map="xargs -n1"
 alias myip="curl -s api.ipify.org"
 alias ipc="myip | copy_cmd"
 
-# Folder managemant
-# alias d='dirs -v'
-# alias 1='pu'
-# alias 2='pu -2'
-# alias 3='pu -3'
-# alias 4='pu -4'
-# alias 5='pu -5'
-# alias 6='pu -6'
-# alias 7='pu -7'
-# alias 8='pu -8'
-# alias 9='pu -9'
-# alias pu='() { pushd $1 &> /dev/null; dirs -v; }'
-# alias po='() { popd &> /dev/null; dirs -v; }'
-
 alias cdot="cd ~/.dotfiles"
 
 alias cdd="cd ~/dev"
