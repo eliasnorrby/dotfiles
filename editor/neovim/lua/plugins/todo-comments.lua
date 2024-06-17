8f359c05eda4f76f6796071e60fcce453c838093
Elias Norrby <elias.norrby@gmail.com>
2024-06-17 10:21:02 +0200

    feat(vim): add navigation mappings for todo comments

--- editor/neovim/lua/plugins/todo-comments.lua
+++ editor/neovim/lua/plugins/todo-comments.lua
@@ -2,4 +2,20 @@ return {
   'folke/todo-comments.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = true,
+  keys = {
+    {
+      ']t',
+      function()
+        require('todo-comments').jump_next()
+      end,
+      desc = 'Next todo comment',
+    },
+    {
+      '[t',
+      function()
+        require('todo-comments').jump_prev()
+      end,
+      desc = 'Previous todo comment',
+    },
+  }
 }
--- editor/neovim/lua/plugins/unimpaired.lua
+++ editor/neovim/lua/plugins/unimpaired.lua
@@ -4,6 +4,13 @@ return {
   config = function()
     local wk = require('which-key')
     local uwk = require('unimpaired-which-key')
+    -- The ]t, [t mappings are overwritten for todo-comments
+    if uwk.normal_mode[']'] then
+      uwk.normal_mode[']'].t = nil
+    end
+    if uwk.normal_mode['['] then
+      uwk.normal_mode['['].t = nil
+    end
     wk.register(uwk.normal_mode)
     wk.register(uwk.normal_and_visual_mode, { mode = { 'n', 'v' } })
   end,
