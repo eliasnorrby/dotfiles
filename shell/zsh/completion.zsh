74f9de878672aeca83d6c6e6c974b1e26f193e41
Elias Norrby <elias.norrby@gmail.com>
2024-07-12 11:44:57 +0200

    refactor(zsh): remove legacy pnpm completion

--- shell/zsh/completion.zsh
+++ shell/zsh/completion.zsh
@@ -5,9 +5,6 @@ if type brew &>/dev/null; then
   fpath=( $HOMEBREW_PREFIX/share/zsh/site-functions $fpath )
 fi
 
-# tabtab source for packages (pnpm)
-# uninstall by removing these lines
-[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
 # tabtab source for packages (netlify)
 [[ -f ~/.config/tabtab/__tabtab.zsh ]] && . ~/.config/tabtab/__tabtab.zsh || true
 
