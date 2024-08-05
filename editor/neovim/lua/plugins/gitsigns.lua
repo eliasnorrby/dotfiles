4a88b9d73b7ba37653f196e997472c5aa700db79
Elias Norrby <elias.norrby@gmail.com>
2024-08-05 12:06:59 +0200

    refactor(vim): rewrite gitsigns mappings

--- editor/neovim/lua/plugins/gitsigns.lua
+++ editor/neovim/lua/plugins/gitsigns.lua
@@ -2,60 +2,75 @@ return {
   'lewis6991/gitsigns.nvim',
   opts = function()
     local wk = require('which-key')
+    local gitsigns = require('gitsigns')
     return {
       on_attach = function(bufnr)
-        wk.register({
-          h = {
-            name = '+gitsigns',
-            s = { '<cmd>Gitsigns stage_hunk<CR>', 'Stage Hunk' },
-            r = { '<cmd>Gitsigns reset_hunk<CR>', 'Reset Hunk' },
-            S = { '<cmd>Gitsigns stage_buffer<CR>', 'Stage Buffer' },
-            u = { '<cmd>Gitsigns undo_stage_hunk<CR>', 'Undo Stage Hunk' },
-            R = { '<cmd>Gitsigns reset_buffer<CR>', 'Reset Buffer' },
-            p = { '<cmd>Gitsigns preview_hunk<CR>', 'Preview Hunk' },
-            b = {
-              '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
-              'Blame Line',
+        wk.add({
+          {
+            buffer = bufnr,
+            { '<leader>h', group = '+gitsigns' },
+            { '<leader>hs', gitsigns.stage_hunk, desc = 'Stage hunk' },
+            { '<leader>hr', gitsigns.reset_hunk, desc = 'Reset hunk' },
+            { '<leader>hS', gitsigns.stage_buffer, desc = 'Stage buffer' },
+            { '<leader>hu', gitsigns.undo_stage_buffer, desc = 'Undo stage hunk' },
+            { '<leader>hR', gitsigns.reset_buffer, desc = 'Reset buffer' },
+            { '<leader>hp', gitsigns.preview_hunk, desc = 'Preview hunk' },
+            {
+              '<leader>hb',
+              function()
+                gitsigns.blame_line({ full = true })
+              end,
+              desc = 'Blame line',
             },
-            d = { '<cmd>Gitsigns diffthis<CR>', 'Diff This' },
-            D = {
-              '<cmd>lua require"gitsigns".diffthis("~")<CR>',
-              'Diff This (Word)',
+            { '<leader>hd', gitsigns.diffthis, desc = 'Diff this' },
+            {
+              '<leader>hD',
+              function()
+                gitsigns.diffthis('~')
+              end,
+              desc = 'Diff this (word)',
+            },
+            -- { '<leader>t', group = '+gitsigns' },
+            { '<leader>tb', gitsigns.toggle_current_line_blame, desc = 'Toggle blame' },
+            { '<leader>td', gitsigns.toggle_deleted, desc = 'Toggle deleted' },
+          },
+          {
+            buffer = bufnr,
+            mode = 'v',
+            { '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>', desc = 'Stage hunk' },
+            { '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>', desc = 'Reset hunk' },
+          },
+          {
+            buffer = bufnr,
+            mode = { 'o', 'x' },
+            { 'ih', ':<C-U>Gitsigns select_hunk<CR>', desc = 'inner hunk' },
+          },
+          {
+            buffer = bufnr,
+            {
+              ']c',
+              function()
+                if vim.wo.diff then
+                  vim.cmd.normal({ ']c', bang = true })
+                else
+                  gitsigns.nav_hunk('next')
+                end
+              end,
+              desc = 'Next hunk',
+            },
+            {
+              '[c',
+              function()
+                if vim.wo.diff then
+                  vim.cmd.normal({ '[c', bang = true })
+                else
+                  gitsigns.nav_hunk('prev')
+                end
+              end,
+              desc = 'Previous hunk',
             },
           },
-          t = {
-            b = {
-              '<cmd>Gitsigns toggle_current_line_blame<CR>',
-              'Toggle Blame',
-            },
-            d = { '<cmd>Gitsigns toggle_deleted<CR>', 'Toggle Deleted' },
-          },
-        }, { prefix = '<leader>', buffer = bufnr })
-
-        wk.register({
-          [']c'] = {
-            "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'",
-            'Next Hunk',
-          },
-          ['[c'] = {
-            "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'",
-            'Previous Hunk',
-          },
-        }, { buffer = bufnr, expr = true })
-
-        wk.register({
-          h = {
-            s = { '<cmd>Gitsigns stage_hunk<CR>', 'Stage Hunk' },
-            r = { '<cmd>Gitsigns reset_hunk<CR>', 'Reset Hunk' },
-          },
-        }, { prefix = '<leader>', buffer = bufnr, mode = 'v' })
-
-        wk.register({
-          ['ih'] = { ':<C-U>Gitsigns select_hunk<CR>', 'inner hunk' },
-        }, { mode = 'o', buffer = bufnr })
-        wk.register({
-          ['ih'] = { ':<C-U>Gitsigns select_hunk<CR>', 'inner hunk' },
-        }, { mode = 'x', buffer = bufnr })
+        })
       end,
     }
   end,
