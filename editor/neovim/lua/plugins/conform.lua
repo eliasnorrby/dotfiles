8a388b93e498008cda65a46fa4ccaf899e862e08
Elias Norrby <elias.norrby@gmail.com>
2024-08-28 14:05:12 +0200

    feat(vim): add toggle for format-on-save
    Inspired by repository recipe:
    https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save

--- editor/neovim/lua/plugins/conform.lua
+++ editor/neovim/lua/plugins/conform.lua
@@ -11,6 +11,12 @@ return {
       mode = '',
       desc = 'Format buffer',
     },
+    {
+      '<leader>tf',
+      '<cmd>ConformAutoFormatToggle<cr>',
+      mode = 'n',
+      desc = 'Toggle autoformat-on-save',
+    },
   },
   opts = {
     formatters_by_ft = {
@@ -31,9 +37,22 @@ return {
       terraform = { 'terraform_fmt' },
       xml = { 'xmllint' },
     },
-    format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
+    format_on_save = function()
+      -- Disable with a global variable
+      if vim.g.disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = 'fallback' }
+    end,
   },
   init = function()
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+    vim.api.nvim_create_user_command('ConformAutoFormatToggle', function()
+      vim.g.disable_autoformat = not vim.g.disable_autoformat
+      print('Autoformat-on-save ' .. (vim.g.disable_autoformat and 'disabled' or 'enabled'))
+    end, {
+      desc = 'Toggle autoformat-on-save',
+    })
   end,
 }
