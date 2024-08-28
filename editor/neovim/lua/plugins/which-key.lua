c215c0bbd838f37e22580cbe7c9f530a7beb4f87
Elias Norrby <elias.norrby@gmail.com>
2024-08-28 14:08:32 +0200

    feat(vim): add <leader>? which-key mapping

--- editor/neovim/lua/plugins/which-key.lua
+++ editor/neovim/lua/plugins/which-key.lua
@@ -5,12 +5,26 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show({ global = false })
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
+    },
+  },
   opts = {
     icons = {
       separator = '•',
     },
     win = {
       border = 'rounded',
+    },
+    triggers = {
+      { '<auto>', mode = 'nixsotc' },
+      { 'r', mode = 'n' },
+      { 'c', mode = 'n' },
     },
   },
 }
