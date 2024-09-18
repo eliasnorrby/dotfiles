6ee7a929b6f69c79978f056250bfe0f727f23a64
Elias Norrby <elias.norrby@gmail.com>
2024-09-18 15:46:58 +0200

    feat(vim): use minimal tabby config

--- editor/neovim/lua/plugins/tabby.lua
+++ editor/neovim/lua/plugins/tabby.lua
@@ -3,6 +3,43 @@ return {
   event = 'VimEnter',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('tabby.tabline').use_preset('active_wins_at_tail')
+    local theme = {
+      fill = 'TabLineFill',
+      head = 'TabLine',
+      current_tab = 'TabLineSel',
+      tab = 'TabLine',
+      win = 'TabLine',
+      tail = 'TabLine',
+    }
+
+    require('tabby').setup({
+      line = function(line)
+        return {
+          {
+            { '   ', hl = theme.tab },
+            line.sep(' ', theme.tab, theme.fill),
+          },
+          line.tabs().foreach(function(tab)
+            local hl = tab.is_current() and theme.current_tab or theme.tab
+            return {
+              ' ',
+              tab.number(),
+              tab.name() ~= '' and ' ' or '',
+              tab.name(),
+              ' ',
+              line.sep(' ', hl, theme.fill),
+              hl = hl,
+            }
+          end),
+        }
+      end,
+      option = {
+        tab_name = {
+          name_fallback = function()
+            return ''
+          end,
+        },
+      },
+    })
   end,
 }
