fcda2c2947cfeb1b01b647b921c47d1052e74d50
Elias Norrby <elias.norrby@gmail.com>
2024-11-11 09:47:27 +0100

    feat(vim): add harpoon
    For quick access to common files like schema.graphql, schema.prisma.

---
+++ editor/neovim/lua/plugins/harpoon.lua
@@ -0,0 +1,58 @@
+---@type LazyPluginSpec
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local wk = require('which-key')
+    local harpoon = require('harpoon')
+    harpoon.setup()
+
+    -- basic telescope configuration
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+
+    wk.add({
+      {
+        { '<leader>j', group = '+harpoon' },
+        {
+          '<C-p>',
+          function()
+            toggle_telescope(harpoon:list())
+          end,
+          desc = 'Open harpoon window',
+        },
+        {
+          '<leader>jo',
+          function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = 'Toggle quick menu',
+        },
+        {
+          '<leader>jt',
+          function()
+            harpoon:list():add()
+          end,
+          desc = 'Add file',
+        },
+      },
+    })
+  end,
+}
