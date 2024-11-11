aeadf7a65db98196472f3ae71144f585a75c1d7b
Elias Norrby <elias.norrby@gmail.com>
2024-11-11 09:44:38 +0100

    refactor(fzf): exclude remote branches in branch binding

--- shell/zsh/fzf.zsh
+++ shell/zsh/fzf.zsh
@@ -143,7 +143,7 @@ gf() {
 
 gb() {
   is_in_git_repo || return
-  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  git branch --color=always | grep -v '/HEAD\s' | sort |
   fzf-down --ansi --multi --tac --preview-window right:70% \
     --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -200' |
   sed 's/^..//' | cut -d' ' -f1 |
