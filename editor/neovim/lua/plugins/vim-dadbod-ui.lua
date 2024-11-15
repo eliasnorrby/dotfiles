87c6806d1c9eb9d6f660a6f2f662740e0fdc3116
Elias Norrby <elias.norrby@gmail.com>
2024-11-15 12:09:55 +0100

    feat(vim): do not use vim-notify for dadbod-ui
    Not very nice without noice.

--- editor/neovim/lua/plugins/vim-dadbod-ui.lua
+++ editor/neovim/lua/plugins/vim-dadbod-ui.lua
@@ -13,7 +13,7 @@ return {
   init = function()
     vim.g.db_ui_execute_on_save = 0
     vim.g.db_ui_use_nerd_fonts = 1
-    vim.g.db_ui_use_nvim_notify = 1
+    vim.g.db_ui_use_nvim_notify = 0
     vim.g.db_ui_win_position = 'right'
 
     vim.cmd([[
