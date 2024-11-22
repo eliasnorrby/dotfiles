9ca7f71b8dd7a12d81b35d3052bcd1287ca701b3
Elias Norrby <elias.norrby@gmail.com>
2024-11-22 08:17:45 +0100

    feat(vim): disable vim-dadbod-ui help message

--- editor/neovim/lua/plugins/vim-dadbod-ui.lua
+++ editor/neovim/lua/plugins/vim-dadbod-ui.lua
@@ -15,6 +15,7 @@ return {
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_use_nvim_notify = 0
     vim.g.db_ui_win_position = 'right'
+    vim.g.db_ui_show_help = 0
 
     vim.cmd([[
       augroup SQLSetup
