08661b2874b9773afc8324b209ffc348473007c2
Elias Norrby <elias.norrby@gmail.com>
2025-01-02 09:29:10 +0100

    feat(vim): add plugin with markdown checkbox commands

--- editor/neovim/lua/plugins/markdown.lua
+++ editor/neovim/lua/plugins/markdown.lua
@@ -1,5 +1,23 @@
+---@type LazySpec
 return {
-  'MeanderingProgrammer/markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  ft = 'markdown',
+  {
+    'tadmccorkle/markdown.nvim',
+    name = 'markdown-tools.nvim',
+    ft = 'markdown',
+    opts = {
+      on_attach = function(bufnr)
+        local wk = require('which-key')
+
+        wk.add({
+          { '<leader>mt', '<cmd>MDTaskToggle<cr>', desc = 'toggle checkbox', buffer = bufnr },
+          { '<CR>', '<cmd>MDTaskToggle<cr>', desc = 'toggle checkbox', buffer = bufnr },
+        })
+      end,
+    },
+  },
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    ft = 'markdown',
+  },
 }
