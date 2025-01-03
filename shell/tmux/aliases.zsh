948bf238d21e439624103d6f3efd0f1a671cd367
Elias Norrby <elias.norrby@gmail.com>
2025-01-03 11:18:38 +0100

    feat(ghostty): add binding for quick terminal
    Adapt tmux attach script to return early if the default session is
    already attached to.

--- shell/tmux/aliases.zsh
+++ shell/tmux/aliases.zsh
@@ -3,6 +3,9 @@
 ialias tmux='tmux -f "$TMUX_HOME/tmux.conf"'
 
 tmux_attach_default() {
+  if tmux list-sessions -F '#S #{?session_attached,attached,not attached}' | grep -q 'default attached'; then
+    return
+  fi
   tmux attach -t default 2>/dev/null || tmux new -s default
 }
 alias ta="tmux_attach_default"
