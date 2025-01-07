765d4c617044e201e408d99067fdfa7d50e85a19
Elias Norrby <elias.norrby@gmail.com>
2025-01-07 13:09:15 +0100

    feat(vim): edit default oil keybinds
    Free up C-h and C-l for window navigation.

--- editor/neovim/lua/plugins/oil.lua
+++ editor/neovim/lua/plugins/oil.lua
@@ -3,7 +3,23 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    keymaps = {
+      ['g?'] = { 'actions.show_help', mode = 'n' },
+      ['<CR>'] = 'actions.select',
+      ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
+      ['<C-x>'] = { 'actions.select', opts = { horizontal = true } },
+      ['<C-t>'] = { 'actions.select', opts = { tab = true } },
+      ['<C-p>'] = 'actions.preview',
+      ['-'] = { 'actions.parent', mode = 'n' },
+      ['_'] = { 'actions.open_cwd', mode = 'n' },
+      ['gs'] = { 'actions.change_sort', mode = 'n' },
+      ['gx'] = 'actions.open_external',
+      ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+      ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+    },
+    use_default_keymaps = false,
+  },
   lazy = true,
   keys = {
     {
