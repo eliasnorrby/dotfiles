8d7009c8d95fe6fc6a2f8a7267a3ad82b8fe7c31
Elias Norrby <elias.norrby@gmail.com>
2025-09-24 01:04:22 +0200

    feat(vim): integrate copilot with blink.cmp
    That is, hide during completion.

--- editor/neovim/lua/plugins/copilot.lua
+++ editor/neovim/lua/plugins/copilot.lua
@@ -28,5 +28,19 @@ return {
         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(';', true, false, true), 'n', false)
       end
     end)
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuOpen',
+      callback = function()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuClose',
+      callback = function()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
   end,
 }
