6c9223cf5218456b8cebe74dcc38854593edbd96
Elias Norrby <elias.norrby@gmail.com>
2025-09-25 21:07:43 +0200

    fix(vim): update bashls config
    Seems the format I was using is ~4 years old.

--- editor/neovim/after/lsp/bashls.lua
+++ editor/neovim/after/lsp/bashls.lua
@@ -1,11 +1,9 @@
 return {
-  cmd_env = {
-    -- lsp-config defaults will disable recursive scanning
-    GLOB_PATTERN = '**/*@(.sh|.inc|.bash|.command)',
+  default_config = {
+    settings = {
+      bashIde = {
+        globPattern = '**/*@(.sh|.inc|.bash|.command)',
+      },
+    },
   },
-  root_dir = function(fname)
-    -- lsp-config will use util.path.dirname, which will be the directory
-    -- containing the file - we won't scan anything in parent directories.
-    return require('lspconfig').util.root_pattern('.git')(fname) or vim.fs.dirname(fname)
-  end,
 }
