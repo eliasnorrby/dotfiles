88dca274d93e2a876406848416a01361e5aec1e9
Elias Norrby <elias.norrby@gmail.com>
2025-10-10 12:28:41 +0200

    feat(vim): add keybind to yank file path in oil

--- editor/neovim/lua/plugins/oil.lua
+++ editor/neovim/lua/plugins/oil.lua
@@ -17,6 +17,17 @@ return {
       ['gx'] = 'actions.open_external',
       ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
       ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+      ['yp'] = { 'actions.yank_entry', mode = 'n' },
+      ['<leader><space>'] = {
+        function()
+          require('telescope.builtin').find_files({
+            cwd = require('oil').get_current_dir(),
+          })
+        end,
+        mode = 'n',
+        desc = 'Find files in current directory',
+        nowait = true,
+      },
     },
     use_default_keymaps = false,
     lsp_file_methods = {
