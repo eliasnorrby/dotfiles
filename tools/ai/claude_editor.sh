98a47fd2d780179ab9e413528a383d663cabf609
Elias Norrby <elias.norrby@gmail.com>
2025-10-16 15:58:32 +0200

    conf(ai): style claude editor popup

--- tools/ai/claude_editor.sh
+++ tools/ai/claude_editor.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 if [[ -n "$TMUX" ]]; then
-  tmux display-popup -E -b rounded -T 'Prompt' "nvim" "$@"
+  tmux display-popup -E -b rounded -T 'Prompt' -S 'fg=blue' "nvim" "+set nonumber norelativenumber" "$@"
 else
   nvim "$@"
 fi
