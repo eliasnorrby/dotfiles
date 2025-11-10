5b888f1b2312065d4f4228f13b02948ff59e947e
Elias Norrby <elias.norrby@gmail.com>
2025-11-10 15:03:43 +0100

    feat(tmux): add wip http popup

---
+++ shell/tmux/scripts/toggle_http_popup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Smart toggle for http popup
+# Wrapper around toggle_stateful_popup with http-specific settings
+
+exec toggle_stateful_popup \
+  --session=http \
+  --title=HTTP \
+  --color=magenta \
+  -d '#{pane_current_path}' \
+  -w 80% \
+  -h 80% \
+  zsh
