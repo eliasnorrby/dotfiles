b58460012cb0a797adad83db1126cb7bfda8643e
Elias Norrby <elias.norrby@gmail.com>
2025-11-11 08:57:38 +0100

    feat(vim): add custom folding for http files

--- editor/neovim/after/ftplugin/http.lua
+++ editor/neovim/after/ftplugin/http.lua
@@ -49,3 +49,29 @@ vim.keymap.set(
   next_param,
   { noremap = true, silent = true, desc = 'Jump to next {param}, delete, and insert' }
 )
+
+-----------------------------------------------------------
+-- Folding
+-----------------------------------------------------------
+
+-- Use marker-based folding with ### as the marker
+vim.opt_local.foldmethod = 'expr'
+vim.opt_local.foldexpr = 'v:lua.HttpFoldExpr(v:lnum)'
+vim.opt_local.foldtext = 'v:lua.HttpFoldText()'
+
+-- Fold expression: start a fold at lines beginning with ###
+function _G.HttpFoldExpr(lnum)
+  local line = vim.fn.getline(lnum)
+  if line:match('^###%s+') then
+    return '>1' -- Start a fold
+  end
+  return '=' -- Continue current fold level
+end
+
+-- Custom fold text: show only the request name
+function _G.HttpFoldText()
+  local line = vim.fn.getline(vim.v.foldstart)
+  local name = line:match('^###%s+(.+)') or 'Request'
+  local line_count = vim.v.foldend - vim.v.foldstart + 1
+  return string.format('### %s  [%d lines] ', name, line_count)
+end
