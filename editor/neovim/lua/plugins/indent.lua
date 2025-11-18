77176c94c0a16d9542b298afedc8772c1403f941
Elias Norrby <elias.norrby@gmail.com>
2025-11-18 09:14:54 +0100

    feat(vim): add blink.indent

---
+++ editor/neovim/lua/plugins/indent.lua
@@ -0,0 +1,28 @@
+return {
+  'saghen/blink.indent',
+  --- @module 'blink.indent'
+  --- @type blink.indent.Config
+  opts = {
+    static = {
+      enabled = false,
+      char = '┊',
+    },
+    scope = {
+      char = '│',
+      highlights = { 'BlinkIndent' },
+    },
+  },
+  init = function()
+    local indent = require('blink.indent')
+    local wk = require('which-key')
+    wk.add({
+      {
+        '<leader>oi',
+        function()
+          indent.enable(not indent.is_enabled())
+        end,
+        { desc = 'Toggle indent guides' },
+      },
+    })
+  end,
+}
