86a0e239c9400b0480326cd6d444c58c63b8fed7
Elias Norrby <elias.norrby@gmail.com>
2025-11-19 14:33:05 +0100

    feat(nvim): configure obsidian.nvim

--- editor/neovim/lua/plugins/obsidian.lua
+++ editor/neovim/lua/plugins/obsidian.lua
@@ -1,10 +1,47 @@
 return {
   'epwalsh/obsidian.nvim',
+  ---@module 'obsidian'
+  ----@type obsidian.config.ClientOpts
   opts = {
-    dir = '~/obsidian',
     disable_frontmatter = true,
-    -- completion = {
-    --   nvim_cmp = true,
-    -- },
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/vaults/personal',
+      },
+      {
+        name = 'bemlo',
+        path = '~/vaults/bemlo',
+      },
+    },
+    daily_notes = {
+      folder = 'dailies',
+      date_format = '%Y-%m-%d',
+      alias_format = '%B %-d, %Y',
+      default_tags = { 'daily-notes' },
+      template = nil,
+    },
+    ui = {
+      enable = false,
+    },
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      -- vim.fn.jobstart({"open", url})  -- Mac OS
+      -- vim.fn.jobstart({"xdg-open", url})  -- linux
+      vim.ui.open(url) -- need Neovim 0.10.0+
+    end,
+    mappings = {
+      ['<cr>'] = {
+        action = function()
+          return require('obsidian').util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+    templates = {
+      folder = '_templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+    },
   },
 }
