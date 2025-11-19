fa8b51e400f4c43b072c82a028d57a6afd198eb4
Elias Norrby <elias.norrby@gmail.com>
2025-11-19 14:33:30 +0100

    feat(tmux): add obsidian popup

---
+++ shell/tmux/scripts/toggle_obsidian_popup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Smart toggle for obsidian popup
+# Wrapper around toggle_stateful_popup with obsidian-specific settings
+
+exec toggle_stateful_popup \
+  --session=obsidian \
+  --title=Obsidian \
+  --color=purple \
+  -x R \
+  -w '40%' \
+  -h '90%' \
+  -d "$HOME/vaults/bemlo" \
+  nvim
