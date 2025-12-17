b40c0ccc73dc722bb3fc31b9620b9a66d73d41a7
Elias Norrby <elias.norrby@gmail.com>
2025-12-17 01:21:45 +0100

    fix(gcloud): fix completion sourcing on arch

--- dev/gcloud/completions.zsh
+++ dev/gcloud/completions.zsh
@@ -2,5 +2,5 @@ if [[ "$(_os)" == "macos" ]]; then
   source "$HOMEBREW_PREFIX/share/google-cloud-sdk/path.zsh.inc"
   source "$HOMEBREW_PREFIX/share/google-cloud-sdk/completion.zsh.inc"
 elif [[ "$(_os)" == "arch" ]]; then
-  source /opt/google-cloud-sdk/completion.zsh.inc
+  source /opt/google-cloud-cli/completion.zsh.inc
 fi
