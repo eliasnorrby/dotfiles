a9c78e8f0e43157eeb142e5ac1713480ea1cbccc
Elias Norrby <elias.norrby@gmail.com>
2025-12-20 00:08:08 +0100

    feat(vim): add claude code aliases

--- editor/neovim/aliases.zsh
+++ editor/neovim/aliases.zsh
@@ -4,3 +4,5 @@ alias vf="nvim +'Telescope find_files'"
 alias vp='nvim package.json'
 alias vg='nvim .gitignore'
 alias vlog='nvim +GV'
+alias vc='nvim +ClaudeCode'
+alias vcc='nvim +ClaudeCodeContinue'
