5e014a70da1db18109064755d9b7a20e911ea424
Elias Norrby <elias.norrby@gmail.com>
2026-01-06 02:15:31 +0100

    refactor(gtk): move gsettings to script

---
+++ wm/gtk/gsettings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'catppuccin-mocha-dark'
+gsettings set org.gnome.desktop.interface cursor-size 24
