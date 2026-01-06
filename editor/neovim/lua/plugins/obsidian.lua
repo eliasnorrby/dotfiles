02545bfc3c5baff7b3eeae458eb6f13208f9c33f
Elias Norrby <elias.norrby@gmail.com>
2026-01-06 02:16:22 +0100

    conf(vim): remove deprecated obsidian config

--- editor/neovim/lua/plugins/obsidian.lua
+++ editor/neovim/lua/plugins/obsidian.lua
@@ -26,6 +26,7 @@ return {
     },
     open = {
       func = function(uri)
+        -- TODO: adjust for non-macOS systems
         vim.ui.open(uri, { cmd = { 'open', '-a', '/Applications/Obsidian.app' } })
       end,
     },
@@ -49,12 +50,6 @@ return {
     ui = {
       enable = false,
     },
-    follow_url_func = function(url)
-      -- Open the URL in the default web browser.
-      -- vim.fn.jobstart({"open", url})  -- Mac OS
-      -- vim.fn.jobstart({"xdg-open", url})  -- linux
-      vim.ui.open(url) -- need Neovim 0.10.0+
-    end,
     templates = {
       folder = '_templates',
       date_format = '%Y-%m-%d',
