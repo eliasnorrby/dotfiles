648b81018c2c131b7413e933c25a85d361900eee
Elias Norrby <elias.norrby@gmail.com>
2026-01-08 11:46:49 +0100

    feat(notifications): add swaync and use instead of mako

--- wm/hyprland/reload_services.sh
+++ wm/hyprland/reload_services.sh
@@ -3,7 +3,14 @@
 # Reload Hyprland and related services
 
 hyprctl reload
-makoctl reload
+
+if command -v makoctl >/dev/null 2>&1; then
+  makoctl reload
+fi
+if command -v swaync-client >/dev/null 2>&1; then
+  swaync-client --reload-config
+  swaync-client --reload-css
+fi
 pkill -USR2 waybar
 
 notify-send "Hyprland Reloaded" "Hyprland and related services have been reloaded."
