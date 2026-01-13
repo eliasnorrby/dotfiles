7eb611af98e7365883d84ec3f41033939d89daf8
Elias Norrby <elias.norrby@gmail.com>
2026-01-13 11:52:02 +0100

    refactor(arch): add chrome wrappers
    To reduce duplication and to properly expand $HOME.

---
+++ base/arch/chrome-personal.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec google-chrome-stable --class=ChromePersonal "$@"
---
+++ base/arch/chrome-work.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec google-chrome-stable --class=ChromeWork --user-data-dir="$HOME/.config/google-chrome-work" "$@"
