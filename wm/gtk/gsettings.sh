6ad60068ddf563c25dc8251864b31728d717b806
Elias Norrby <elias.norrby@gmail.com>
2026-01-13 15:59:13 +0100

    fix(gtk): use correct cursor theme name

--- wm/gtk/gsettings.sh
+++ wm/gtk/gsettings.sh
@@ -2,5 +2,5 @@
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-gsettings set org.gnome.desktop.interface cursor-theme 'catppuccin-mocha-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'catppuccin-macchiato-dark-cursors'
 gsettings set org.gnome.desktop.interface cursor-size 24
