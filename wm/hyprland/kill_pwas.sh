8c55ba3d3782cf70f9330c9f0b6403478898a573
Elias Norrby <elias.norrby@gmail.com>
2026-01-16 21:41:25 +0100

    revert: "feat(hyprland): kill pwas before suspend"
    Oh well, it seems they're just as CPU intensive at all times. I've just
    been wearing a headset and haven't heard the fans spin up.

--- wm/hyprland/kill_pwas.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Close Chrome PWA windows before suspend to prevent post-resume CPU spikes
-
-# Chrome PWAs have window class pattern: chrome-{id}-Default
-hyprctl clients -j |
-  jq -r '.[] | select(.class | startswith("chrome-")) | .address' |
-  while read -r addr; do
-    hyprctl dispatch closewindow "address:$addr"
-  done
