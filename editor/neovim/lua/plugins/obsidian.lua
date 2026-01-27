051ac33d3d49d921dfff1bf61e5c5ae5de621c27
Elias Norrby <elias.norrby@gmail.com>
2026-01-27 15:12:46 +0100

    feat(obsidian): add neovim obsidian desktop file popup

--- editor/neovim/lua/plugins/obsidian.lua
+++ editor/neovim/lua/plugins/obsidian.lua
@@ -6,6 +6,9 @@ return {
     'BufReadPre ' .. vim.fn.expand('~') .. '/vaults/*/*.md',
     'BufNewFile ' .. vim.fn.expand('~') .. '/vaults/*/*.md',
   },
+  cmd = {
+    'Obsidian',
+  },
   ---@module 'obsidian'
   ----@type obsidian.config.ClientOpts
   opts = {
