b55e3913b1c8d3544b6f2a0dffa12bf3b38bce04
Elias Norrby <elias.norrby@gmail.com>
2026-02-06 10:00:46 +0100

    conf(aliases): shadow zoxide's z to launch project picker without args

--- tools/yazi/aliases.zsh
+++ tools/yazi/aliases.zsh
@@ -1,4 +1,14 @@
 # sets up zoxide (z)
 eval "$(zoxide init zsh)"
 
+if type projects >/dev/null 2>&1; then
+  z() {
+    if [[ -n "$1" ]]; then
+      __zoxide_z "$@"
+    else
+      projects
+    fi
+  }
+fi
+
 alias y="yazi"
